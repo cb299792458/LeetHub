7546a545068f2e9714acd8a670dbf50deb060ad5
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-11-19 13:32:54 -0500

    Time: 118 ms (61.25%), Space: 45.1 MB (7.61%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let obj = {};
+    let ans = [];
+    nums.forEach((num,i)=>{
+        if(obj[target-num] || obj[target-num] === 0){
+            ans = [obj[target-num],i]
+        }
+        obj[num] = i;
+    });
+    return ans;
+};
