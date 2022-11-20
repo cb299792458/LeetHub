97e5198997651606a49635f58b0dd3ae61ec8bc0
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-11-20 11:05:01 -0500

    Time: 174 ms (22.91%), Space: 45.9 MB (38.33%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let counts = {};
+    for(let c of s){
+        if(counts[c] !== undefined){
+            counts[c]++;
+        } else {
+            counts[c] = 1;
+        }
+    }
+    for(let c of t){
+        if(counts[c] !== undefined){
+            counts[c]--;
+        } else {
+            counts[c] = -1;
+        }
+    }
+    console.log(counts);
+    let nums = Object.values(counts).filter((num)=>{
+        return num != 0;
+    })
+    return nums.length === 0;
+};
