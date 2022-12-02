473e77a3dc3c3123933ed1201cfc4a66ab712f89
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-02 14:34:49 -0500

    Time: 87 ms (93.16%), Space: 51.7 MB (93.83%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,13 +14,6 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     
-    // if(root.val === p.val || root.val === q.val){
-    //     return root;
-    // }
-    // if((root.val < p.val && root.val > q.val) || (root.val < q.val && root.val > p.val)){
-    //     return root;
-    // }
-    
     if(root.val < p.val && root.val < q.val){
         return lowestCommonAncestor(root.right,p,q)
     }
