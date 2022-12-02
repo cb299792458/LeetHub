3ed8d3cac6bb08d97566e7c6d8e2ac6289ffb13e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-02 14:53:15 -0500

    Time: 121 ms (54.90%), Space: 46.9 MB (80.73%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    if(!root){ return true }
+    return nodeIsBalanced(root) && isBalanced(root.right) && isBalanced(root.left)
+};
+
+const height = (root) => {
+        if(!root){ return 0 }
+
+    if(!root.left && !root.right){
+        return 1;
+    } else {
+        return Math.max(height(root.left),height(root.right)) + 1;
+    }
+}
+
+const nodeIsBalanced = function(root) {
+        if(!root){ return true }
+
+    const left = root.left ? height(root.left) : 0;
+    const right = root.right ? height(root.right) : 0;
+    return Math.abs( left - right ) < 2;
+}
