663e94c53e706197232e1971d6a41ba714733ee3
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-02 14:54:30 -0500

    Time: 77 ms (93.13%), Space: 46.9 MB (80.73%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.js
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -18,15 +18,10 @@ var isBalanced = function(root) {
 const height = (root) => {
     if(!root){ return 0 }
 
-    // if(!root.left && !root.right){
-    //     return 1;
-    // } else {
-        return Math.max(height(root.left),height(root.right)) + 1;
-    // }
+    return Math.max(height(root.left),height(root.right)) + 1;
 }
 
 const nodeIsBalanced = function(root) {
-    // if(!root){ return true }
 
     const left = root.left ? height(root.left) : 0;
     const right = root.right ? height(root.right) : 0;
