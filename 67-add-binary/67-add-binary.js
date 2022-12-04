53fe57555ba40fa946ab4b10773d88e18241a9f8
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-04 18:49:31 -0500

    Time: 104 ms (55.94%), Space: 44.3 MB (27.78%) - LeetHub

---
+++ 67-add-binary/67-add-binary.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    const digitsA = a.split('').map(n=>parseInt(n)).reverse();
+    const digitsB = b.split('').map(n=>parseInt(n)).reverse();
+
+    let place = 0;
+    let carry = 0;
+    let answer = '';
+    
+    while(digitsA[place] !== undefined || digitsB[place] !== undefined || carry){
+        let sum = (digitsA[place] ? 1 : 0) + (digitsB[place] ? 1 : 0) + carry;
+        
+        if(sum > 1){
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+        
+        if(sum % 2 === 1){
+            answer = 1 + answer;
+        } else {
+            answer = 0 + answer;
+        }
+        
+        
+        place++;
+    }
+    return answer || '0';
+};
