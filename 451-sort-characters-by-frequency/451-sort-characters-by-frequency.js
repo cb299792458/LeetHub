181775d7ec1bfa7d18b4598b450330c3783a6d66
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-13 13:56:58 -0500

    Time: 301 ms (5.05%), Space: 49.8 MB (41.24%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
@@ -3,16 +3,93 @@
  * @return {string}
  */
 var frequencySort = function(s) {
-    let counts = {};
-    s.split('').forEach((char)=>{
-        counts[char] = counts[char] ? counts[char] + 1 : 1;
+    
+    let buckets = new Array(62).fill(0);
+    
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwyxz1234567890'.split('')
+    for(let input of s){
+        buckets[chars.indexOf(input)]++
+    }
+    
+    // console.log(buckets);
+    // buckets is an array
+    // the index is where in chars the char is found
+    // the element is the count of times that char appears in s
+    
+    let output = '';
+    let charAmount = {};
+    for(let i=0; i<buckets.length; i++){
+        let char = chars[i];
+        let amount = buckets[i];
+        
+        if(amount === 0){continue}
+        
+        // for(let j = 0; j < amount; j++){
+            // output += char;
+            charAmount[char] = [char,amount];
+        // }
+        
+        
+    }
+    let toSort = Object.values(charAmount);
+    
+    let sorted = toSort.sort((a,b)=>{
+        if(a[1] > b [1]){
+            return -1;
+        }
+        return 1;
     });
-    let countArr = Object.entries(counts).sort((a,b)=>{return b[1] - a[1]})
-    let ans = '';
-    countArr.forEach((arr)=>{
-       for(let i = 0; i < arr[1]; i++){
-           ans += arr[0];
-       } 
-    });
-    return ans;
+    
+    for(let element of sorted){
+        let [char,amount] = element;
+        for(let i=0;i<amount;i++){
+            output += char;
+        }
+    }
+    
+
+    return output;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // let counts = {};
+    // s.split('').forEach((char)=>{
+    //     counts[char] = counts[char] ? counts[char] + 1 : 1;
+    // });
+    // let countArr = Object.entries(counts).sort((a,b)=>{return b[1] - a[1]})
+    // let ans = '';
+    // countArr.forEach((arr)=>{
+    //    for(let i = 0; i < arr[1]; i++){
+    //        ans += arr[0];
+    //    } 
+    // });
+    // return ans;
 };
