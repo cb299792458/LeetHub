609b9c1aa6f55e00c56d01e9465525530b2a4a8d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-12 19:41:16 -0500

    Time: 308 ms (61.96%), Space: 51.6 MB (69.02%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} homepage
+ */
+var BrowserHistory = function(homepage) {
+    this.history =[homepage]
+    this.currentPage = 0
+};
+/** 
+ * @param {string} url
+ * @return {void}
+ */
+BrowserHistory.prototype.visit = function(url) {
+    this.history = this.history.slice(0, this.currentPage+1)
+    this.history.push(url)
+    this.currentPage ++
+};
+/** 
+ * @param {number} steps
+ * @return {string}
+ */
+BrowserHistory.prototype.back = function(steps) {
+    if(this.currentPage >= steps){
+        this.currentPage -= steps
+    }else {
+        this.currentPage = 0
+    }
+    return this.history[this.currentPage]
+    
+};
+/** 
+ * @param {number} steps
+ * @return {string}
+ */
+BrowserHistory.prototype.forward = function(steps) {
+    if(this.history.length > steps + this.currentPage){
+        this.currentPage += steps
+    }else {
+        this.currentPage = this.history.length -1
+    }
+    return this.history[this.currentPage]
+};
