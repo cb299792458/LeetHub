056e0aba2d53fcb485d151d5cdf9b10c2303a616
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-14 13:50:06 -0500

    Time: 132 ms (16.39%), Space: 50.2 MB (5.04%) - LeetHub

---
+++ 112-path-sum/112-path-sum.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum, currSum = 0) {
+    if(!root) return false
+    currSum += root.val
+    if(targetSum === currSum && !root.left && !root.right) return true
+    console.log(root.val, currSum)
+    return hasPathSum(root.left, targetSum, currSum) || hasPathSum(root.right, targetSum, currSum)
+};
