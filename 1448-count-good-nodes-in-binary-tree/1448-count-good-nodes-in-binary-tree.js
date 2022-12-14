4f61e05c52f15677e760571538d352fb5a0c38c0
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-14 14:05:26 -0500

    Time: 153 ms (92.02%), Space: 65.2 MB (91.65%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -11,24 +11,7 @@
  * @return {number}
  */
 var goodNodes = function(root,largest = -Infinity) {
-//     bfs
-//     let queue = [root];
-    // let largest = -Infinity;
-//     let goodNodes = 0;
-    
-//     while(queue.length>0){
-//         let curr = queue.shift();
-//         if(curr.val >= largest){
-//             goodNodes++;
-//         }
-//         largest = Math.max(largest,curr.val);
-//         if(curr.left){queue.push(curr.left)}
-//         if(curr.right){queue.push(curr.right)}
-        
-//     }
-    
-    // return goodNodes;
-    
+
     // dfs
     let goodNode = 0;
     
