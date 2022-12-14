10a11daa2aed72e1b31ea215918b88d155917462
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-14 14:07:52 -0500

    Time: 70 ms (89.57%), Space: 43.8 MB (55.90%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    //dfs
+    let stack = [{node: root, level: 1}]
+    let answer = new Map
+    let level = 1
+    let result = []
+
+    while(stack.length){
+        let curr = stack.pop()
+        if(curr.node){
+            if(!answer.has(curr.level)) answer.set(curr.level, curr.node.val)
+
+            if(curr.node.left) stack.push({node: curr.node.left, level: curr.level + 1})
+            if(curr.node.right) stack.push({node: curr.node.right, level: curr.level + 1})
+
+        }
+    }
+
+    answer.forEach(node => {
+        result.push(node)
+    })
+
+    return result
+};
