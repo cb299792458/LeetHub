e31acadc10d4c034f502c27f68e9f5592198a109
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-14 11:22:27 -0500

    Time: 1610 ms (5.00%), Space: 61.1 MB (5.02%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let area = 0;
+    
+    while(left < right){
+        console.log(left,right)
+        area = Math.max(area,(right-left)*Math.min(height[left],height[right]));
+        if(height[left] > height[right]){
+            right--;
+        } else {
+            left++;
+        }
+    }
+    
+    return area;
+};
