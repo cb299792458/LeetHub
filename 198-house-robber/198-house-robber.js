211b85a8e03d1d846c9d427c5117f8d95a1358dc
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-14 18:07:37 -0500

    Time: 62 ms (93.54%), Space: 41.9 MB (52.66%) - LeetHub

---
+++ 198-house-robber/198-house-robber.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if(nums.length<2){return nums[0]}
+    let max = [nums[0],Math.max(nums[0],nums[1])]
+    for(let i = 2; i < nums.length; i++){
+        max[i] = Math.max(max[i-2] + nums[i], max[i-1]);
+    }
+    console.log(max);
+    return Math.max(max.at(-1),max.at(-2));
+    
+};
