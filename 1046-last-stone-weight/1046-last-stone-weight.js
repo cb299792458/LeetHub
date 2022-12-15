f46f1825534093f6578cfbd23f1e7ebf655702f1
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-14 19:25:58 -0500

    Time: 75 ms (85.62%), Space: 42.8 MB (75.97%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -5,6 +5,18 @@
 var lastStoneWeight = function(stones) {
     
     // make a heap, much faster
+    const queue = new MaxPriorityQueue();
+    
+    for (stone of stones) queue.enqueue(stone)
+    
+    while (queue.size() > 1) {
+        let first = queue.dequeue().element;
+        let second = queue.dequeue().element;
+        if (first !== second) queue.enqueue(first-second)
+    }
+    
+    return queue.size() === 0 ? 0 : queue.front().element
+
     
     while(stones.length>1){
         stones = stones.sort((a,b)=>{return a-b});
