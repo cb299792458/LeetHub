3f99c726fcfc459885ae8c71e093ba1ad34ee57d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-15 17:52:31 -0500

    Time: 280 ms (64.61%), Space: 55 MB (58.54%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.js
@@ -0,0 +1,44 @@
+var updateMatrix = function(mat) {
+    const queue = [];
+
+    const res = [];
+    res.push(new Array(mat.length))
+    for (let row = 0; row < mat.length; row++){
+        res[row] = new Array(mat[0].length)
+        for (let col = 0; col < mat[0].length; col++){
+            
+            if (mat[row][col] === 0) {
+                res[row][col] = 0;
+                queue.push([row, col, 0])
+            }
+        }
+    }
+    console.log(res)
+
+    let directions = [[1,0], [0,1], [-1,0], [0,-1]]
+    while (queue.length > 0) {
+        const [row, col, level] = queue.shift();
+
+        if (inBounds(mat, row, col)) {
+
+            for (let dir of directions) {
+                const newRow = row + dir[0];
+                const newCol = col + dir[1];
+                if (inBounds(mat, newRow, newCol) && res[newRow][newCol] === undefined) {
+                    queue.push([newRow, newCol, level+1])
+                    res[newRow][newCol] = level+1
+                }
+                // res[row][col] = res[newRow][newCol]
+            }
+        }
+    }
+
+    return res
+};
+
+const inBounds = (grid, row, col) => {
+    const rowInBounds = 0 <= row && row < grid.length;
+    const colInBounds = 0 <= col && col < grid[0].length;
+    return rowInBounds && colInBounds
+}
+
