cb046e4b9ba77b4a2fd937e4d24072bbd7efa170
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-16 19:04:04 -0500

    Time: 73 ms (84.36%), Space: 43.2 MB (32.82%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.js
+++ 63-unique-paths-ii/63-unique-paths-ii.js
@@ -4,11 +4,11 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
 
     let stepsToEnd = [...Array(row)].map(ele => Array(col).fill(null));
 
+    // fill in the last square with a 1 if it's open, and a zero if it's a rock
     if(obstacleGrid[row-1][col-1] === 0){
         stepsToEnd[row-1][col-1] = 1;
     } else {
         stepsToEnd[row-1][col-1] = 0;
-
     }
     
 
@@ -18,7 +18,7 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
             let paths = 0;
             if (!outOfBounds(obstacleGrid, row, col + 1)) paths += stepsToEnd[row][col + 1];
             if (!outOfBounds(obstacleGrid, row + 1, col)) paths += stepsToEnd[row + 1][col];
-            stepsToEnd[row][col] = stepsToEnd[row][col] || paths;
+            stepsToEnd[row][col] = stepsToEnd[row][col] || paths; // don't overwrite the last square;
         }
     }
 
