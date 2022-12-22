326d48573522a1cf58391334010b67944735b824
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-22 13:50:29 -0500

    Time: 88 ms (83.62%), Space: 46.1 MB (82.65%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    
+    let graph = buildGraph(prerequisites);
+    let res = true;
+    
+    const checkLoop = (id) => {
+        if(seen.has(id)){
+            return false;
+        }
+        seen.add(id);
+        if(graph[id] !== undefined){
+            for(let prereq of graph[id]){
+                if(!checkLoop(prereq)) return false;
+            }
+        }
+        seen.delete(id);
+        graph[id] = undefined;
+        return true;
+    };
+    
+    let seen = new Set;
+    for(let course = 0; course < numCourses; course++){
+        if(!checkLoop(course)) return false;
+    }
+    return res;
+};
+
+function buildGraph(prerequisites) {
+    const graph = {};
+    for(let prereq of prerequisites){
+        const [a,b] = prereq;
+        
+        if(!(a in graph)) graph[a] = [];
+        graph[a].push(b);
+        
+    }
+    return graph;
+}
