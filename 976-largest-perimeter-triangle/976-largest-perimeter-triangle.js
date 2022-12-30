8ddca24933625d4b2eb104b1fc236cbaa2238d23
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2022-12-30 12:30:34 -0500

    Time: 103 ms (91.65%), Space: 45.3 MB (72.36%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -4,7 +4,7 @@
  */
 var largestPerimeter = function(nums) {
     nums = nums.sort((a,b) => {return a-b});
-    console.log(nums)
+    // console.log(nums)
     for(let i = nums.length - 1; i > 1; i--){
         if(nums[i] < nums[i-1] + nums[i-2]) return nums[i] + nums[i-1] + nums[i-2]
     }
