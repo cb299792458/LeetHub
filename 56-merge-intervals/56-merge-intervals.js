feb0a800292de6ec72d3d296139aaf1b3908c668
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-05 13:42:56 -0500

    Time: 124 ms (65.60%), Space: 49.8 MB (33.19%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals = intervals.sort((a,b)=>a[0]-b[0]);
+    intervals.push([Infinity,Infinity])
+    
+    let res = [];
+    let prev;
+    for(let interval of intervals){
+        if(!prev){
+            prev = interval;
+            continue;
+        }
+        
+        if(prev[1]<interval[0]){
+            res.push(prev);
+            prev = interval;
+            continue;
+        } else {
+            if(prev[1]<interval[1]){
+                prev[1] = interval[1];
+            }
+        }
+    }
+    
+    return res;
+};
