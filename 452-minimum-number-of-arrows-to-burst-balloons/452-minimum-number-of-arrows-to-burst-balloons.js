2ba7cfe725f8a17a76e34485bff8238e5b021630
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-05 12:28:49 -0500

    Time: 650 ms (5.51%), Space: 76.4 MB (6.90%) - LeetHub

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    points = points.sort((a,b)=>{
+        return a[1] - b[1];
+    })
+    console.log(points);
+    
+    let arrows = [-Infinity];
+    for(let i=0;i<points.length;i++){
+        if(arrows.at(-1)>points[i][1] || arrows.at(-1)<points[i][0]){
+            arrows.push(points[i][1])
+        }
+    }
+    console.log(arrows);
+    
+    return arrows.length-1
+};
