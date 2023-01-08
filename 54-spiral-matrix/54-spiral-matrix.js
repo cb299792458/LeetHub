641efa567f6922a809343ab6e4dff4f5cea50746
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-07 19:24:29 -0500

    Time: 61 ms (88.74%), Space: 42.3 MB (17.80%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.js
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -8,6 +8,7 @@ var spiralOrder = function(matrix) {
     const rows = matrix.length;
     const cols = matrix[0].length;
     
+    // Set up boundary around matrix
     let out = [];
     for(let i = -1; i <= rows; i ++){
         for(let j = -1; j <= cols; j ++){
@@ -20,10 +21,14 @@ var spiralOrder = function(matrix) {
     let [r,c] = [0,0];
     let res = [];
     while(res.length < rows*cols){
+        
+        // Add new element to res
         if(!out.includes(`${r},${c}`)){
             out.push(`${r},${c}`);
             res.push(matrix[r][c]);
         }
+        
+        
         [r,c] = [r+dirs[dirIdx%4][0],c+dirs[dirIdx%4][1]];
         if(out.includes(`${r},${c}`)){
             [r,c] = [r-dirs[dirIdx%4][0],c-dirs[dirIdx%4][1]];
