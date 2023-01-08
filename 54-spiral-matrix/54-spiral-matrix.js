da3aae540a0818b38833a19d49f8700f576564ad
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-08 14:58:25 -0500

    Time: 73 ms (61.91%), Space: 42 MB (45.36%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.js
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -9,11 +9,11 @@ var spiralOrder = function(matrix) {
     const cols = matrix[0].length;
     
     // Set up boundary around matrix
-    let out = [];
+    let out = new Set;
     for(let i = -1; i <= rows; i ++){
         for(let j = -1; j <= cols; j ++){
             if(i===-1 || j===-1 || i===rows || j===cols){
-                out.push(`${i},${j}`);
+                out.add(`${i},${j}`);
             }
         }
     }
@@ -23,14 +23,14 @@ var spiralOrder = function(matrix) {
     while(res.length < rows*cols){
         
         // Add new element to res, if it isn't already there
-        if(!out.includes(`${r},${c}`)){
-            out.push(`${r},${c}`);
+        if(!out.has(`${r},${c}`)){
+            out.add(`${r},${c}`);
             res.push(matrix[r][c]);
         }
         
         // Set new coordinates, if they are out of bounds, set them back and rotate direction using modulo
         let newCoords = [r+dirs[dirIdx%4][0],c+dirs[dirIdx%4][1]];
-        if(out.includes(`${newCoords[0]},${newCoords[1]}`)){
+        if(out.has(`${newCoords[0]},${newCoords[1]}`)){
             dirIdx++;
         } else {
             [r,c] = newCoords;
