ba123c04c274f8f1e4cbeea661e4266c6ab21ce1
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-10 16:00:00 -0500

    Time: 4481 ms (6.91%), Space: 74.2 MB (5.05%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const res = [];
+    const set = new Set;
+    
+    for(let i=0;i<nums.length;i++){
+        const first = nums[i];
+        const map = {};
+        for(let j=i+1;j<nums.length;j++){
+            const second=nums[j];
+            if(map[second]===undefined){
+                map[0-first-second]=second;
+            } else{
+                let ans = [first, second, map[second]];
+                ans = ans.sort((a,b)=>a-b);
+                
+                if(set.has(`${ans[0]},${ans[1]},${ans[2]}`)){
+                    
+                } else {
+                    res.push(ans);
+                    set.add(`${ans[0]},${ans[1]},${ans[2]}`)
+                }
+                
+            }
+        }
+    }
+    
+    return res;
+    
+};
