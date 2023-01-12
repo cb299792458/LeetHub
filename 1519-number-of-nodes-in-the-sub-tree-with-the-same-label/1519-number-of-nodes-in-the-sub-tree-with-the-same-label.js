5075e36ad0ded1d8d3d0bba790047824d252d6f2
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-12 11:23:01 -0500

    Time: 1741 ms (33.33%), Space: 137.2 MB (33.33%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {string} labels
+ * @return {number[]}
+ */
+var countSubTrees = function(n, edges, labels) {
+    
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const graph = [];
+    for(let i=0; i<n; i++){
+        graph[i]=[];
+    }
+    for(const edge of edges){
+        graph[edge[0]].push(edge[1]);
+        graph[edge[1]].push(edge[0]);
+    }
+    // console.log(graph)
+    
+    const res = [];
+    const memo = {};
+    
+    const countLabel = (node) => {
+
+        // if(memo[node]) return memo[node];
+        if(memo[node]) return new Array(26).fill(0);
+        memo[node] = new Array(26).fill(0);
+        
+        for(let neighbor of graph[node]){
+            const neighborCounts = countLabel(neighbor);
+            for(let index in neighborCounts){
+                memo[node][index] += neighborCounts[index];
+            }
+        }
+        
+        memo[node][alphabet.indexOf(labels[node])]++;
+        
+        // console.log('checking ', node, memo)
+        res[node] = memo[node][alphabet.indexOf(labels[node])];
+        return memo[node];
+    };
+    
+    countLabel(0)
+    return res
+    
+};
