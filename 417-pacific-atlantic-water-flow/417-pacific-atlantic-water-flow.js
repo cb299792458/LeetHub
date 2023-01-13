280231758b826338863d019516f97fe43e064fe5
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-13 18:38:23 -0500

    Time: 150 ms (62.44%), Space: 50.9 MB (62.59%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
@@ -18,24 +18,6 @@ var pacificAtlantic = function(heights) {
     const dfs = (r,c,memo) => {
         const coords = `${r},${c}`;
 
-//         if(memo[coords] === true) return true;
-//         if(memo[coords] === false) return;
-//         if(memo[coords] === 'checking') return;
-//         memo[coords] = 'checking';
-
-//         for(let dir of dirs){
-//             const newRow = r + dir[0];
-//             const newCol = c + dir[1];
-//             if(outOfBounds(newRow,newCol)) continue;
-//             if(heights[r][c] < heights[newRow][newCol]) continue;
-            
-//             if(dfs(newRow,newCol,memo)===true){
-//                 memo[coords] = true;
-//                 return true;
-//             };
-//         }
-//         memo[coords] = false;
-//         return false;
         if(memo[coords]) return;
         memo[coords] = true;
         for(let dir of dirs){
@@ -47,36 +29,6 @@ var pacificAtlantic = function(heights) {
             dfs(newRow,newCol,memo);
         }
     };
-    
-//     const bfs = (startRow,startCol,memo) => {
-//         let q = [[startRow,startCol]];
-        
-//         while(q.length){
-            
-//             const [r,c] = q.shift();
-//             if(outOfBounds(r,c)) continue;
-//             const coords = `${r},${c}`;
-
-//             if(memo[coords] === true) return true;
-//             if(memo[coords] === false) return;
-//             if(memo[coords] === 'checking') return;
-//             memo[coords] = 'checking';
-            
-//             for(let dir of dirs){
-//                 const newRow = r + dir[0];
-//                 const newCol = c + dir[1];
-//                 if(outOfBounds(newRow,newCol)) continue;
-//                 if(heights[r][c] < heights[newRow][newCol]) continue;
-
-//                 if(dfs(newRow,newCol,memo)===true){
-//                     memo[coords] = true;
-//                     return true;
-//                 };
-//             }
-//         }
-//     };
-    
-
     
     const res = [];
     
@@ -90,8 +42,6 @@ var pacificAtlantic = function(heights) {
     
     for(let i = 0; i <= maxRow; i++){
         for(let j = 0; j <= maxCol; j++){
-            // dfs(i,j,pSquares);
-            // dfs(i,j,aSquares);
             const coords = `${i},${j}`;
             
             if(pSquares[coords]===true&&aSquares[coords]===true){
