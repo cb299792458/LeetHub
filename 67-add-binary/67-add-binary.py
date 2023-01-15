035e492f5b40feb47c5426c00fa1cb1c8ebe6d63
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-15 12:05:37 -0500

    Time: 35 ms (81.05%), Space: 14 MB (22.30%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,39 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        
+        def a_digit(i):
+            a_start = len(a)-1
+            if(a_start-i<0):
+                return '0'
+            return a[a_start-i]
+        
+        def b_digit(i):
+            b_start = len(b)-1
+            if(b_start-i<0):
+                return '0'
+            return b[b_start-i]
+        
+        
+        res = ''
+        carry = 0
+        for i in range( 1 + max(len(a),len(b)) ):
+
+            if(a_digit(i)=='1'):
+                carry += 1
+                # print(carry)
+            if(b_digit(i)=='1'):
+                carry += 1
+                
+            if(carry%2 == 1):
+                res = '1' + res
+            else:
+                res = '0' + res
+            
+            if(carry>1):
+                carry = 1
+            else:
+                carry = 0
+            
+        if(res[0]=='0'):
+            res = res[1:]
+        return res
