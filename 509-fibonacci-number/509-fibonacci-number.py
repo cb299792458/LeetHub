6d628e2f1413de0b6d8f4ee417788b1ac3af10b5
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-17 17:33:52 -0500

    Time: 41 ms (61.15%), Space: 13.8 MB (95.26%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,12 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # def helper(n):  
-        #     if(n<2):
-        #         return n
-        #     return helper(n-1) + helper(n-2)
-        # return helper(n)
+
         memo = [0,1]
         for i in range(2,n+1):
             memo.append(memo[i-1] + memo[i-2])
-            # print(i)
         return memo[n]
