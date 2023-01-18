928de9a101c40692cf456fca3437d4b30fbb1e0b
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-18 18:17:37 -0500

    Time: 111 ms (5.13%), Space: 46.4 MB (5.13%) - LeetHub

---
+++ 394-decode-string/394-decode-string.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    console.log(s)
+    let res = ''
+    let nums = '0123456789';
+    
+    let i = 0;
+    let repeats = 0;
+    
+    let prefix = ''
+    
+    while(!nums.includes(s[i]) && s[i]){
+        i++;
+    }
+    
+    prefix = s.slice(0,i);
+    res += prefix;
+    
+    if(i===s.length) return res;
+    
+    console.log(res,i)
+    while(nums.includes(s[i])){
+        repeats *= 10;
+        repeats += parseInt(s[i]);
+        i++;
+    } 
+    
+    let j = i;
+    let openBrackets = 1;
+    while(openBrackets > 0){
+        j++;
+        // console.log(s[j])
+        if(s[j]==='[') openBrackets++;
+        if(s[j]===']') openBrackets--;
+    }
+
+    for(let k=0;k<repeats;k++){
+        res += decodeString(s.slice(i+1,j));
+    }
+    
+    res += decodeString(s.slice(j+1));
+    
+    return res;
+};
