195ef2ca5529baa69bdcf82b5f46431ca533fd5a
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-20 13:53:15 -0500

    Time: 1130 ms (24.24%), Space: 49.6 MB (39.39%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.js
+++ 1169-invalid-transactions/1169-invalid-transactions.js
@@ -4,19 +4,19 @@
  */
 var invalidTransactions = function(transactions) {
 
-    transactions.sort((a,b)=>{
+    transactions.sort((a,b)=>{ // Sort by time
         return a.split(',')[1] - b.split(',')[1]
     });
     
     let res = new Set;
-    for(let i=0;i<transactions.length;i++){
+    for(let i=0;i<transactions.length;i++){ // check each transaction
         const [n,t,a,c] = transactions[i].split(',');
-        if(a>1000){
+        if(a>1000){ // check for amount
             res.add(i);
         }
         
         let j = i + 1;
-        while(transactions[j]){
+        while(transactions[j]){ // check for diff country
             const [n2,t2,a2,c2] = transactions[j].split(',');
             if( n===n2 && c!==c2 && t2-t <= 60){
                 res.add(i);
@@ -26,7 +26,7 @@ var invalidTransactions = function(transactions) {
         }
     }
     
-    return transactions.filter((transaction,index) => {
+    return transactions.filter((transaction,index) => { // Use set of indices to avoid repeat
         return res.has(index);
     });
 };
