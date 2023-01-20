51141bcf980152af5da78d3c62fb05ffc9bb5102
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-20 16:52:30 -0500

    Time: 848 ms (45.62%), Space: 19 MB (25.82%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trusters = set()
+        trusted = [None]
+        
+        for i in range(1,n+1):
+            trusted.append(set())
+            
+        for truster,trustee in trust:
+            trusters.update([truster])
+            trusted[trustee].update([truster])
+            
+        print(trusters,trusted)
+        for person in range(1,n+1):
+            print(person, person in trusters, trusted[person])
+            if person not in trusters and len(trusted[person]) == n-1:
+                return person
+        
+        return -1
