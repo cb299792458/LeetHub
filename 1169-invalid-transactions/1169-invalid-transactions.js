4a4f42ca59bc455233ec7d0db5419def830f0bfc
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-20 19:12:27 -0500

    Time: 410 ms (36.30%), Space: 49.8 MB (25.19%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.js
+++ 1169-invalid-transactions/1169-invalid-transactions.js
@@ -16,7 +16,7 @@ var invalidTransactions = function(transactions) {
         }
         
         let j = i + 1;
-        while(transactions[j]){ // check for diff country
+        while(transactions[j] && transactions[j].split(',')[1] <= parseInt(t)+60 ){ // check for diff country
             const [n2,t2,a2,c2] = transactions[j].split(',');
             if( n===n2 && c!==c2 && t2-t <= 60){
                 res.add(i);
