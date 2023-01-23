87887fa607d7d6ecebb531a3cd8907b4fd52c66d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-23 14:26:01 -0500

    Time: 77 ms (78.44%), Space: 46.5 MB (38.39%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -5,7 +5,7 @@
 var canJump = function(nums) {
     let maxReach = 0;
     let i = 0;
-    while(i<=maxReach && i < nums.length){
+    while(i<=maxReach){
         maxReach = Math.max(maxReach,nums[i]+i);
         if(maxReach >= nums.length - 1) return true
         i++;
