ebedc47fc2b7d1e1b61009d0f2c1cace67352aa1
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-23 13:50:27 -0500

    Time: 5532 ms (5.03%), Space: 53.5 MB (5.01%) - LeetHub

---
+++ 55-jump-game/55-jump-game.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let dp = new Array(nums.length).fill(false);
+    dp[0] = true;
+    
+    for(let i=0;i<nums.length;i++){
+        if(dp[i]){
+            for(let j=1;j<=nums[i];j++){
+                dp[i+j] = true;
+            }
+        }
+    }
+    
+    return dp.at(-1);
+};
