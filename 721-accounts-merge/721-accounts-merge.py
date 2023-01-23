b3570447a6dcc3ccc89d17a8b88f312d115af037
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-22 20:40:37 -0500

    Time: 7651 ms (5.03%), Space: 17.9 MB (81.16%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.py
@@ -0,0 +1,37 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        skip = set()
+        res = []
+        
+        changed = True
+        while changed:
+            changed = False
+            
+            for i in range(len(accounts)):
+                if i in skip:
+                    continue
+                name = accounts[i][0]
+                emails = accounts[i][1:]
+                
+                
+                for j in range(1,len(accounts[i])):
+                    email = accounts[i][j]
+                    
+                    for k in range(i+1,len(accounts)):
+                        if k in skip:
+                            continue
+                        if email in accounts[k]:
+                            changed = True
+                            skip.add(k)
+                            
+                            emails += accounts[k][1:]
+                emails = list(set(emails))
+                emails.sort()
+                            
+                accounts[i] = [name] + emails
+                
+                
+        # print(accounts,skip)
+                    
+            
+        return [accounts[i] for i in range(0,len(accounts)) if i not in skip]
