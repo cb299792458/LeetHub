995ea5ef52336ae2530dd35cd4ac339011192f63
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-25 15:01:23 -0500

    Time: 605 ms (64.78%), Space: 118.3 MB (11.08%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
@@ -1,36 +1,40 @@
 
-var RandomizedSet = function() {
-    this.set = new Set
-};
+class RandomizedSet{
+    constructor(){
+        this.set = new Set
+    }
+    
 
-/** 
- * @param {number} val
- * @return {boolean}
- */
-RandomizedSet.prototype.insert = function(val) {
-    let res = !this.set.has(val);
-    this.set.add(val);
-    return res;
-};
 
-/** 
- * @param {number} val
- * @return {boolean}
- */
-RandomizedSet.prototype.remove = function(val) {
-    let res = this.set.has(val);
-    this.set.delete(val);
-    return res;
-};
+    /** 
+     * @param {number} val
+     * @return {boolean}
+     */
+    insert = function(val) {
+        let res = !this.set.has(val);
+        this.set.add(val);
+        return res;
+    };
 
-/**
- * @return {number}
- */
-RandomizedSet.prototype.getRandom = function() {
-    const entries = [...this.set]
-    return entries[Math.floor(entries.length*Math.random())]
-};
+    /** 
+     * @param {number} val
+     * @return {boolean}
+     */
+    remove = function(val) {
+        let res = this.set.has(val);
+        this.set.delete(val);
+        return res;
+    };
 
+    /**
+     * @return {number}
+     */
+    getRandom = function() {
+        const entries = [...this.set]
+        return entries[Math.floor(entries.length*Math.random())]
+    };
+
+}
 /** 
  * Your RandomizedSet object will be instantiated and called as such:
  * var obj = new RandomizedSet()
