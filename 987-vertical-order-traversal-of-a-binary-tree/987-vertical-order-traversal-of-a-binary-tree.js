1ff999914b989aa94ef07400410123fd53096010
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-25 14:57:54 -0500

    Time: 72 ms (62.64%), Space: 44 MB (76.92%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalTraversal = function(root) {
+    const nums = {};
+    
+    function fillMap(node,col,row){
+        if(!node) return;
+        if(!nums[col]) nums[col] = {};
+        if(!nums[col][row]) nums[col][row] = [];
+        nums[col][row].push(node.val);
+        
+        fillMap(node.left,col-1,row+1);
+        fillMap(node.right,col+1,row+1);
+    }
+    
+    fillMap(root,0,0);
+    let colIndices = Object.keys(nums);
+    colIndices.sort((a,b)=>parseInt(a)-parseInt(b));
+    // console.log(nums);
+    
+    const ans = [];
+    for(let colIndex of colIndices){
+        let temp = [];
+        
+        let rowIndices = Object.keys(nums[colIndex]);
+        rowIndices.sort((a,b)=>parseInt(a)-parseInt(b));
+        for(let rowIndex of rowIndices){
+            nums[colIndex][rowIndex].sort((a,b)=>a-b)
+            // console.log(nums[colIndex][rowIndex])
+            // temp.push(nums[colIndex][rowIndex])
+            temp = temp.concat(nums[colIndex][rowIndex])
+        }
+        
+        ans.push(temp);
+    }
+    
+    return ans;
+};
