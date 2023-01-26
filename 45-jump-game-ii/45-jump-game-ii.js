37bbf62ccf386d317d5e05fa11cedec2c03dd4e6
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-26 09:18:22 -0500

    Time: 299 ms (21.03%), Space: 44.7 MB (23.60%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const n = nums.length;
+    const memo = [0];
+    
+    for(let i=0;i<n;i++){
+        for(let jumps = nums[i]; jumps > 0; jumps--){
+            if(!memo[i+jumps]){
+                memo[i+jumps] = memo[i]+1;
+            } else {
+                memo[i+jumps] = Math.min(memo[i]+1,memo[i+jumps]);
+            }
+            
+        }
+    }
+    return memo[n-1]
+};
