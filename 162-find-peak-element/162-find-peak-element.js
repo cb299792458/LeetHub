7648af8119638184ea989fe35f6a1efac337a685
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-27 18:50:06 -0500

    Time: 71 ms (50.51%), Space: 42.3 MB (20.69%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.js
+++ 162-find-peak-element/162-find-peak-element.js
@@ -9,7 +9,6 @@ var findPeakElement = function(nums) {
     
     while(left <= right){
         const mid = Math.floor((left+right)/2);
-        console.log(left,right,mid)
         if((nums[mid-1]===undefined || nums[mid]>nums[mid-1])&&(nums[mid+1]===undefined || nums[mid]>nums[mid+1])) return mid;
         if(nums[mid]<nums[mid+1]){
             left = mid+1;
