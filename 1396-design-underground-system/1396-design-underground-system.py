1f921788779afabff67eecd61db8566f59c1e3ad
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-27 17:25:05 -0500

    Time: 259 ms (56.91%), Space: 24.3 MB (52.37%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -0,0 +1,28 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.open_trips = {}
+        self.closed_trips = {}
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.open_trips[id] = {'start': stationName, 't': t}
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        open_trip = self.open_trips[id]
+        del self.open_trips[id]
+        
+        route = open_trip['start'] + '-' + stationName
+        if not route in self.closed_trips:
+            self.closed_trips[route] = {'sum': 0, 'num': 0}
+        self.closed_trips[route]['sum'] += t - open_trip['t']
+        self.closed_trips[route]['num'] += 1
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        return self.closed_trips[startStation+'-'+endStation]['sum']/self.closed_trips[startStation+'-'+endStation]['num']
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
