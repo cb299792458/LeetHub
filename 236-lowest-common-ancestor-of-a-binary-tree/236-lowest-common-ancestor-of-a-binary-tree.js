5e813bfd988d12a86f5f848a9bc5f2b3ce310d8c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-27 10:06:09 -0500

    Time: 3806 ms (5.08%), Space: 51.5 MB (63.96%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(node, p, q, badMemo = new Set) {
+    // if(badMemo.has(node)) return false;
+    
+    const descendant = (node,child)=>{
+        if(!node) return false;
+        if(node.val===child.val) return true;
+        return descendant(node.left,child) || descendant(node.right,child);
+    }
+    let current = node;
+    
+    while(true){
+        if(descendant(current.left,p)&&descendant(current.left,q)){
+            current = current.left;
+        } else if(descendant(current.right,p)&&descendant(current.right,q)){
+            current = current.right;
+        } else break
+    }
+    
+    
+    
+    return current;
+    
+};
