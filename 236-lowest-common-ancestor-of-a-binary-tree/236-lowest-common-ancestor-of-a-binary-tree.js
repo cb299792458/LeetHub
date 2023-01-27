8f8c96eea601650d7b2d3d695a2ccf8c1aaf7bc1
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-27 10:14:00 -0500

    Time: 76 ms (90.53%), Space: 51.5 MB (63.96%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -13,21 +13,28 @@
  */
 var lowestCommonAncestor = function(node, p, q) {
     
-    const descendant = (node,child)=>{
-        if(!node) return false;
-        if(node.val===child.val) return true;
-        return descendant(node.left,child) || descendant(node.right,child);
-    }
-    let current = node;
+//     const descendant = (node,child)=>{
+//         if(!node) return false;
+//         if(node.val===child.val) return true;
+//         return descendant(node.left,child) || descendant(node.right,child);
+//     }
+//     let current = node;
     
-    while(true){
-        if(descendant(current.left,p)&&descendant(current.left,q)){
-            current = current.left;
-        } else if(descendant(current.right,p)&&descendant(current.right,q)){
-            current = current.right;
-        } else break
-    }
+//     while(true){
+//         if(descendant(current.left,p)&&descendant(current.left,q)){
+//             current = current.left;
+//         } else if(descendant(current.right,p)&&descendant(current.right,q)){
+//             current = current.right;
+//         } else break
+//     }
     
-    return current;
+//     return current;
     
+    if(!node) return false;
+    if(node===p || node===q) return node;
+    let left = lowestCommonAncestor(node.left,p,q);
+    let right = lowestCommonAncestor(node.right,p,q);
+    
+    if(left && right) return node;
+    return left ? left : right;
 };
