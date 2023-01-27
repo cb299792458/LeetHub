73b9ae102cf3f023951afe5b53bca50fbbf0734e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-26 20:19:50 -0500

    Time: 928 ms (50.42%), Space: 48.9 MB (18.44%) - LeetHub

---
+++ 79-word-search/79-word-search.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    // const visited = new Set;
+    
+    function search(word,row,col){
+        if(!word) return true;
+        if(row<0||row>=board.length) return false;
+        if(col<0||col>=board[0].length) return false;
+        
+        // const visited = new Set(ovisited)
+        // if(visited.has(`${row},${col}`)) return false;
+        // visited.add(`${row},${col}`);
+        
+        if(board[row][col]!==word[0]) return false;
+        let char = board[row][col]
+        board[row][col] = ' '
+        
+        if(search(word.slice(1),row+1,col)) return true;
+        if(search(word.slice(1),row-1,col)) return true;
+        if(search(word.slice(1),row,col+1)) return true;
+        if(search(word.slice(1),row,col-1)) return true;
+        board[row][col] = char;
+    }
+    
+    for(let i=0;i<board.length;i++){
+        for(let j=0;j<board[0].length;j++){
+            if(search(word,i,j)) return true;
+        }
+    }
+    return false;
+};
