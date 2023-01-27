7059a2880dc07dd7004cc49206223ef2ea0b43fd
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-27 10:42:28 -0500

    Time: 584 ms (21.24%), Space: 49.3 MB (43.90%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+
+    let ans = [];
+    
+    let counts = {};
+    for(let char of p){
+        if(!counts[char]) counts[char] = 0;
+        counts[char]++;
+    }
+    
+    
+    for(let right=0;right<s.length;right++){
+        let left = right - p.length;
+        
+        if(!counts[s[right]]) counts[s[right]] = 0;
+        counts[s[right]]--;
+        
+        if(left>=0){
+            if(!counts[s[left]]) counts[s[left]] = 0;
+            counts[s[left]]++;
+        }
+        
+        if(Object.values(counts).every(ele=>ele===0)){
+            ans.push(left+1)
+        }
+    }
+    
+    
+    
+    return ans;
+};
