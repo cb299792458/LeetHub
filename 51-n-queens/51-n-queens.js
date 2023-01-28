1c71f067048e152f3293ee62db35278a7b8af9b0
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-28 09:16:06 -0500

    Time: 126 ms (31.06%), Space: 50.7 MB (6.31%) - LeetHub

---
+++ 51-n-queens/51-n-queens.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function(n) {
+    const ans = [];
+    
+    let seen = new Set;
+    
+    function parse(grid){
+        let res = [];
+        for(let i=0;i<n;i++){
+            res.push(grid[i].join(''));
+        }
+        return res;
+    }
+    
+    function backtrack(grid,queens=0,rows=new Set,cols=new Set,sumdiags=new Set,difdiags=new Set){
+        let row = queens;
+        if(queens===n){
+            let parsed = parse(grid);
+
+            ans.push(parsed);
+
+            return;
+        }
+        
+        rows = new Set(rows);
+        cols = new Set(cols);
+        sumdiags = new Set(sumdiags);
+        difdiags = new Set(difdiags);
+        grid.push(new Array(n).fill('.'))
+        
+        for(let col=0;col<n;col++){
+            if(rows.has(row)||cols.has(col)||sumdiags.has(row+col)||difdiags.has(row-col)) continue;
+
+            rows.add(row);
+            cols.add(col);
+            sumdiags.add(row+col);
+            difdiags.add(row-col);    
+            grid[row][col] = 'Q';
+            backtrack(grid,queens+1,rows,cols,sumdiags,difdiags);
+
+            rows.delete(row);
+            cols.delete(col);
+            sumdiags.delete(row+col);
+            difdiags.delete(row-col);  
+            grid[row][col] = '.';
+        }
+    }
+    
+    backtrack([],0);
+    
+    return ans;
+    
+};
