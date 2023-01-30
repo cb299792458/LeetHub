763229e68bc98b9327e1d946e29206971c7a5262
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-30 14:21:02 -0500

    Time: 622 ms (77.23%), Space: 101.6 MB (35.79%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.js
@@ -0,0 +1,98 @@
+/**
+ * @param {number} capacity
+ */
+class Node{
+    constructor(key,val,next,prev){
+        this.key = key;
+        this.val = val;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+var LRUCache = function(capacity) {
+    this.capacity = capacity;
+    this.map = new Map;
+    this.head = new Node('head','head');
+    this.tail = new Node('tail','tail');
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if(!this.map.has(key)) return -1;
+    
+    let node = this.map.get(key);
+    
+    let before = node.prev;
+    let after = node.next;
+    before.next = after;
+    after.prev = before
+    
+    let second = this.head.next;
+    this.head.next = node;
+    node.prev = this.head;
+    node.next = second;
+    second.prev = node;
+
+
+    
+    
+    return node.val
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    let node;
+    if(!this.map.has(key)){
+        node = new Node(key, value);
+        this.map.set(key,node);
+    } else {
+        node = this.map.get(key);
+        node.val = value;
+        this.map.set(key,node);
+        
+        let before = node.prev;
+        let after = node.next;
+        before.next = after;
+        after.prev = before
+    }
+    
+    let second = this.head.next;
+    this.head.next = node;
+    node.prev = this.head;
+    node.next = second;
+    second.prev = node;
+    
+    if(this.map.size > this.capacity){
+        let last = this.tail.prev;
+        let nextToLast = last.prev;
+        
+        nextToLast.next = this.tail;
+        this.tail.prev = nextToLast;
+        this.map.delete(last.key);
+    }
+    
+    
+    let c = this.head;
+    // while(c){
+    //     console.log(c.key, c.val);
+    //     c = c.next;
+    // }
+    
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
