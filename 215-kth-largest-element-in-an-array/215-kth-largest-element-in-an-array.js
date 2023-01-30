48cab78d685e0c65be92a1a16a5aae1feb894431
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-30 11:58:48 -0500

    Time: 272 ms (22.79%), Space: 68.2 MB (5.15%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    const pq = new MaxPriorityQueue;
+    
+    for(let num of nums){
+        pq.enqueue(num);
+    }
+    
+    for(let i=1;i<k;i++){
+        pq.dequeue();
+    }
+    
+    return pq.front().element
+};
