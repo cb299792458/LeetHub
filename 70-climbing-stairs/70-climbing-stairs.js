885fdc416e3c250cd06421cde4cb9a8e6980c7e7
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-31 15:46:42 -0500

    Time: 65 ms (68.78%), Space: 41.1 MB (99.14%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let answers = [null,1,2];
+    let answers = {};
+    answers[1] = 1;
+    answers[2] = 2;
     
     for(let i = 3; i <= n; i++){
         answers[i] = answers[i-1] + answers[i-2];
