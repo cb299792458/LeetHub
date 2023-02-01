595cda11548cc3602cbd0f1aec154205fdea2e25
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-01-31 20:11:39 -0500

    Time: 67 ms (73.55%), Space: 43 MB (94.66%) - LeetHub

---
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.js
@@ -0,0 +1,57 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function(head, beforeHead=null, afterTail=null) {
+    if(!head) return null;
+    
+    
+    head.prev = beforeHead;
+    if(beforeHead) beforeHead.next = head;
+    
+    let current = head;
+    
+        if(current.child){ //2 
+        let currentTail = current.next; //null
+            
+        current.next = flatten(current.child, current, currentTail); //2,1,null
+        current.child = null;
+    } 
+    
+    while(current.next){ //3  //8 //12
+        if(current.child){ //7   //11
+            let currentTail = current.next; //4 //9
+            
+            current.next = flatten(current.child, current, currentTail); //(7,3,4)  //11,8,9
+            current.child = null;
+            
+            
+        } else {
+            current = current.next;
+        }
+    }
+    
+    
+
+    
+    if(current) current.next = afterTail;
+    if(afterTail) afterTail.prev = current;
+    
+    // let print = head;
+    // while(print){
+    //     console.log(print);
+    //     print=print.next;
+    // }
+    
+    return head;
+};
