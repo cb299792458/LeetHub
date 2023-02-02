434eb40e763b59a7197f239daa04603941ff5cad
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-01 20:06:17 -0500

    Time: 70 ms (61.73%), Space: 42.4 MB (65.05%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function(words, order) {
+    function sorted(first,second){
+        for(let i=0;i<first.length;i++){
+            console.log(first[i],second[i])
+            if(first[i] && !second[i]) return false;
+            if(order.indexOf(first[i])>order.indexOf(second[i])) return false;
+            if(order.indexOf(first[i])<order.indexOf(second[i])) return true;
+        }
+        return true;
+    }
+    
+    // console.log(sorted('asdfjb','asdfjh'))
+    for(let i=0;i<words.length-1;i++){
+        if(!sorted(words[i],words[i+1])) return false;
+    }
+    return true;
+};
