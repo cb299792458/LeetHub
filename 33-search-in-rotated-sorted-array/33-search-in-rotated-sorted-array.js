445ce64d7681dde680a67d35b4c45f9e48a02f6b
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-08 16:32:43 -0500

    Time: 68 ms (58.15%), Space: 42.2 MB (45.02%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -10,6 +10,7 @@ var search = function(nums, target) {
     while(left<right){
         let mid = Math.floor((left+right)/2);
         if(nums[mid]===target) return mid;
+        
         if(nums[left] <= nums[mid]){
             // left not rotated
             if(nums[left]<=target && target<nums[mid]){
