f7ce03a221826652144fc688b864e3ad76b941c1
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-08 11:04:01 -0500

    Time: 1238 ms (5.66%), Space: 70.6 MB (5.28%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function(accounts) {
+    const owners = {};
+    const emails = [];
+    
+    for(let i in accounts){
+        emails[i] = []
+        for(let j=1;j<accounts[i].length;j++){
+            let email = accounts[i][j];
+            if(!owners[email]) owners[email] = [];
+            owners[email].push(i);
+            emails[i].push(email);
+        }
+    }
+    
+    // console.log(owners);
+    // console.log(emails);
+    
+    let seen = new Set;
+    let mergedAccounts = [];
+    
+    for(let i in accounts){
+        if(seen.has(i)) continue;
+        
+        let userEmails = new Set;
+        let stack = [i];
+        while(stack.length){
+            let current = stack.pop();
+            if(seen.has(current)) continue;
+            seen.add(current);
+            for(let email of emails[current]){
+                userEmails.add(email);
+                // console.log(owners[email])
+                stack = stack.concat(owners[email]);
+            }
+        }
+        // console.log(userEmails)
+        let emailArray = Array.from(userEmails);
+        emailArray.sort((a,b)=>{
+            if(a<b) return -1;
+            return 1;
+        });
+        
+        mergedAccounts.push([accounts[i][0],...emailArray])
+    }
+    return mergedAccounts
+};
