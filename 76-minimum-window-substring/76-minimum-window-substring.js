f279206b4b000ae0956f36ba1e36766b325ebe8c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-08 18:26:20 -0500

    Time: 328 ms (13.83%), Space: 50.8 MB (9.76%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    let counts = {};
+    for(let char of t){
+        if(!counts[char]) counts[char] = 0;
+        counts[char]++;
+    }
+    let correctCounts = 0;    
+    
+    const valid = () => correctCounts === Object.keys(counts).length;
+    let window = false;
+    
+    let left = 0, right = 0;
+
+            if(counts[s[right]]!==undefined){
+                counts[s[right]]--;
+                if(counts[s[right]]<=0) correctCounts++;
+            }    
+    while(left < s.length){
+        // console.log(counts,correctCounts)
+        if(valid()){
+            if(!window||window[1]-window[0]>right-left) window=[left,right];
+            if(counts[s[left]]!==undefined){
+                counts[s[left]]++;
+                if(counts[s[left]] === 1) correctCounts--;
+            }
+            left++
+        } else if(right !== s.length-1){
+            right++;
+            if(counts[s[right]]!==undefined){
+                counts[s[right]]--;
+                if(counts[s[right]]===0) correctCounts++;
+            }
+        } else {
+            if(counts[s[left]]!==undefined){
+                counts[s[left]]++;
+                if(counts[s[left]] === 1) correctCounts--;
+            }
+            left++
+        }
+        
+    }
+    // console.log(window)
+    return window ? s.slice(window[0],window[1]+1) : ''
+};
