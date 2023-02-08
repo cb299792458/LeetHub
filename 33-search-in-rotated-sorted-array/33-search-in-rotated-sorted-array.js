de024622fd65905161a05ee197505a217c86e2c2
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-08 09:59:55 -0500

    Time: 75 ms (31.85%), Space: 44.5 MB (5.18%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length-1;
+    
+    while(left<right){
+        let mid = Math.floor((left+right)/2);
+        console.log(left,mid,right)
+        if(nums[mid]===target) return mid;
+        if(nums[left] <= nums[mid]){
+            // left not rotated
+            if(nums[left]<=target && target<nums[mid]){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            // right not rotated
+            if(nums[mid]<target && target<=nums[right]){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+    }
+    if(nums[left]===target) return left;
+    return -1;
+};
