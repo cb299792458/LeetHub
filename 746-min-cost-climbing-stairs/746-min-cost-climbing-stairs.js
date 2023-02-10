0ce9ffe733aaf99116681fb9973b07a4f4ccd3b4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-10 17:18:26 -0500

    Time: 70 ms (62.21%), Space: 44.3 MB (21.90%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    let mins = [0,0];
+    const mins = [0,0];
     for(let i=2;i<=cost.length;i++) mins.push(Math.min(mins[i-1]+cost[i-1],mins[i-2]+cost[i-2]))
     return mins.at(-1)
 };
