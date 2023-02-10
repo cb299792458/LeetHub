d11df5808b131281d8ddf2dbf8d39d0476f2b295
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-10 17:14:11 -0500

    Time: 80 ms (55.73%), Space: 44.4 MB (57.58%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+var getHint = function(secret, guess) {
+    let bulls = 0;
+    let cows = 0;
+    let s = {};
+    let g = {};
+    for(let i=0;i<secret.length;i++){
+        if(secret[i]===guess[i]){
+            bulls++;
+        } else {
+            s[secret[i]] ||= 0;
+            s[secret[i]]++;
+            g[guess[i]] ||= 0;
+            g[guess[i]]++;
+        }
+    }
+    for(let key of Object.keys(s)){
+        if(g[key]){
+            cows += Math.min(s[key],g[key])
+        }
+    }
+    
+    return bulls + 'A' + cows + 'B'
+};
