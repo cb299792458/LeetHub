05aed9eb3a8cddc8797df3b423b26294f0659079
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-09 20:46:38 -0500

    Time: 326 ms (19.81%), Space: 59.4 MB (9.43%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.js
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.js
@@ -9,18 +9,13 @@ var maxDistance = function(grid) {
     let hasWater = false;
     let seen = new Set()
     
-    
     function fill(r,c,dist){
         if(r<0||c<0||r===grid.length||c===grid.length) return;
         if(seen.has(`${r},${c}`)) return;
         seen.add(`${r},${c}`);
         
-        if(grid[r][c] === 0){
-            hasWater = true;}
-            grid[r][c] = dist;
-        // } else {
-        //     grid[r][c] = Math.min(grid[r][c],dist)
-        // }
+        if(grid[r][c] === 0) hasWater = true;
+        grid[r][c] = dist;
         
         for(let dir of dirs){
             let [nr,nc] = [r+dir[0],c+dir[1]];
@@ -34,7 +29,6 @@ var maxDistance = function(grid) {
     for(let r=0;r<grid.length;r++){
         for(let c=0;c<grid[0].length;c++){
             if(grid[r][c]===1){
-                // fill(r,c,1);
                 queue.push([r,c,1])
                 hasLand = true;
             }
