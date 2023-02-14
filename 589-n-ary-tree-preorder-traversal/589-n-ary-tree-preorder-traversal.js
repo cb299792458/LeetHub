32d83212fbea7c0f71f9b7b1b57e04fc72a971ba
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-14 10:43:33 -0500

    Time: 93 ms (37.22%), Space: 47.4 MB (23.38%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,24 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+    let res = [];
+    if(!root) return res;
+    res = res.concat(root.val);
+
+        
+        for(let child of root.children){
+            res = res.concat(preorder(child));
+        }
+
+    return res;
+};
