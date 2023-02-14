e8e4e700dbfd2b599222805d1cd72d1eb334e96b
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-14 11:53:17 -0500

    Time: 73 ms (43.97%), Space: 42.3 MB (75.46%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    let stringS = [];
+    let stringT = [];
+    
+    for(let char of s){
+        if(char==='#'){
+            stringS.pop();
+        } else {
+            stringS.push(char);
+        }
+    }
+    for(let char of t){
+        if(char==='#'){
+            stringT.pop();
+        } else {
+            stringT.push(char);
+        }
+    }
+    
+    return stringS.join()===stringT.join();
+};
