a1ff810b3d5a6d3a7872b27ac23d35916027e9bf
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-16 10:10:00 -0500

    Time: 1842 ms (5.11%), Space: 82.2 MB (76.39%) - LeetHub

---
+++ 204-count-primes/204-count-primes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    let arr = new Array(n).fill(true);
+    let count = 0;
+    
+    for(let i=2;i<n;i++){
+        if(arr[i]) count++;
+        for(let j=i+i;j<n;j+=i){
+            arr[j] = false;
+        }
+    }
+    return count;
+};
