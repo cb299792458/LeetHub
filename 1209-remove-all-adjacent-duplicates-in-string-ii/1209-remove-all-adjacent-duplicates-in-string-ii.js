ce68e06003237fc072da1da8c062924a56d31d7e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-15 20:59:35 -0500

    Time: 2008 ms (5.01%), Space: 55.1 MB (18.88%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var removeDuplicates = function(s, k) {
+    
+    // make a stack = []
+    let stack = [];
+    
+    // for loop
+    for(let i=0;i<s.length;i++){
+        // put char in the stack
+        let char = s[i];
+        stack.push(char);
+        
+        
+        // check if the last k elements in the stack are the same;
+        if(stack.length >= k){
+            let lastK = stack.slice(stack.length-k);
+            // console.log(lastK)
+            if( lastK.every((element) => element === stack.at(-1)) ){
+                for(let i=0;i<k;i++) stack.pop();
+            } 
+        }
+        
+    }
+    return stack.join('');
+    
+};
+
+
+// deeedbbcccbdaa
+// ddbbcccbdaa
+// ddbbbdaa
+// dddaa
+// aa
+
+// O(n x k)
