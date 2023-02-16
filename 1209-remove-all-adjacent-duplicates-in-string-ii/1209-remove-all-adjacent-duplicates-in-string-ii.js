b8d2e193066067be3ce7162c85de7229a9ff8efc
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-16 08:46:08 -0500

    Time: 73 ms (97.64%), Space: 51 MB (46.02%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -1,31 +1,18 @@
-/**
- * @param {string} s
- * @param {number} k
- * @return {string}
- */
 var removeDuplicates = function(s, k) {
-    
     // make a stack = []
     let stack = [];
-    
-    // for loop
-    for(let i=0;i<s.length;i++){
-        // put char in the stack
-        let char = s[i];
-        stack.push(char);
-        
-        // check if the last k elements in the stack are the same;
-        if(stack.length >= k){
-            let lastK = stack.slice(stack.length-k);
-            if( lastK.every((element) => element === stack.at(-1)) ){
-                for(let i=0;i<k;i++) stack.pop();
-            } 
+    for(let char of s){
+        let lastI = stack.length - 1
+        if(stack.length > 0 && stack[lastI][0] === char){
+            stack[lastI][1]++
+            if(stack[lastI][1] === k) stack.pop()
+        }else{
+            stack.push([char, 1])
         }
-        
     }
-    return stack.join('');
-    
+    let res = ""
+    for(let i = 0; i < stack.length; i++){
+        res += stack[i][0].repeat(stack[i][1])
+    }
+    return res
 };
-
-
-// aa
