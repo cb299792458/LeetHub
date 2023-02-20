da9c7ba4e33d6a7d466a40c089f20136467342ef
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-20 15:42:25 -0500

    Time: 99 ms (36.61%), Space: 49.7 MB (38.36%) - LeetHub

---
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    let vowels = new Set('aeiouAEIOU'.split(''));
+    console.log(vowels)
+    let left=0,right=s.length-1;
+    let arr = s.split('');
+    while(left<right){
+        if(!vowels.has(arr[left])){
+            left++;
+            continue;
+        }
+        if(!vowels.has(arr[right])){
+            right--;
+            continue;
+        }
+        [arr[left],arr[right]] = [arr[right],arr[left]];
+        right--;
+        left++;
+    }
+    return arr.join('');
+};
