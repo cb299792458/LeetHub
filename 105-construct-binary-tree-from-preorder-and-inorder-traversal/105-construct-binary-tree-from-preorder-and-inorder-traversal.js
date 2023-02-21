c76f0afc6a241389d3e45c71ff388b3dced92bee
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-21 18:23:30 -0500

    Time: 172 ms (23.45%), Space: 136.4 MB (5.52%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -16,13 +16,7 @@ var buildTree = function(preorder, inorder) {
     
     let headVal = preorder[0];
     let index = inorder.indexOf(headVal);
-    
-//     let leftInorder = inorder.slice(0,index);
-//     let rightInorder = inorder.slice(index+1);
-    
-//     let leftPreorder = preorder.slice(0,index);
-//     let rightPreorder = preorder.slice(index);
-    
+        
     return new TreeNode(headVal,
                     buildTree(preorder.slice(1,index+1),inorder.slice(0,index)),
                     buildTree(preorder.slice(index+1),inorder.slice(index+1))
