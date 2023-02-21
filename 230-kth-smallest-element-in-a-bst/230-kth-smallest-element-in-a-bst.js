0e834baa7790008a021b289c04a97c64e506c953
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-21 18:34:43 -0500

    Time: 74 ms (79.96%), Space: 48.6 MB (39.77%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
@@ -14,13 +14,16 @@
 var kthSmallest = function(root, k) {
     let nums = []
     function traverse(node){
-        if(!node) return;
-        traverse(node.left);
+        
+        if(nums.length===k) return nums[k-1];
+        if(!node) return false;
+        if(traverse(node.left)) return traverse(node.left);
         nums.push(node.val);
-        traverse(node.right);
+        if(traverse(node.right)) return traverse(node.right);
+        return nums[k-1]
     }
-    traverse(root);
+    // traverse(root);
     // console.log(nums);
-    return nums[k-1];
+    return traverse(root);
     
 };
