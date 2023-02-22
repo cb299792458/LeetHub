8c9f68e578649600009acda4b1e596080bd6994d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-22 09:18:32 -0500

    Time: 96 ms (48.24%), Space: 43.2 MB (38.42%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -6,14 +6,15 @@
 var characterReplacement = function(s, k) {
     let counts = {};
     let left=0,right=-1;res=0;
-    
+    let most = 0;
     while(right<s.length){
-        let most = Math.max(...Object.values(counts),0);
+        // let most = Math.max(...Object.values(counts),0);
         
         if(right-left < k+most){
             right++;
             if(!counts[s[right]]) counts[s[right]] = 0;
             counts[s[right]]++;
+            most = Math.max(most,counts[s[right]])
             
         } else {
             counts[s[left]]--;
