9052916b3f14011c9379b04c1322e4929ce63d8c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-21 20:33:17 -0500

    Time: 1291 ms (5.59%), Space: 44.7 MB (95.80%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.js
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.js
@@ -5,9 +5,12 @@
  * @return {number}
  */
 var getKth = function(lo, hi, k) {
+    let powers = {};
+    
     function power(n){
         let count = 0;
         while(n>1){
+            if(power[n]) return power[n];
             count++;
             if(n%2===0){
                 n /= 2;
@@ -15,11 +18,15 @@ var getKth = function(lo, hi, k) {
                 n = 3*n+1
             }
         }
+        power[n] = count;
         return count;
     }
     
+    
     let arr = [];
     for(let i = lo; i <= hi; i++) arr.push(i);
+    
+    
     arr.sort((a,b)=>{
         let pa = power(a);
         let pb = power(b);
