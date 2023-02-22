4260611e19f4226c6519fe46da6d8af94a6fada6
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-02-22 11:12:59 -0500

    Time: 70 ms (82.35%), Space: 47 MB (27.10%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.js
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -1,45 +1,27 @@
-// /**
-//  * @param {number[]} candidates
-//  * @param {number} target
-//  * @return {number[][]}
-//  */
-// var combinationSum2 = function(candidates, target) {
-//     const ans = [];
-//     const anset = new Set();
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    const ans = [];
+    // const anset = new Set();
+    candidates.sort();
     
-//     const backtrack = (nums, index, t) => {
-//         console.log(nums,index);
-//         if(t<0) return;
-//         if(t===0 && !anset.has(nums.join(''))){
-//             anset.add(nums.join(''));
-//             ans.push(nums);
-//             return;
-//         }
-                
-//         for(let i=index;i<candidates.length;i++){
-//             backtrack([...nums,candidates[i]],i+1,t-candidates[i])
-//         }
-//     };
-    
-//     backtrack([],0,target);
-//     return ans;
-// };
-var combinationSum2 = function(c, target) {
-    c.sort((a,b)=>a-b)
-    let res = []
-
-    let iterate = (index,sum,temp) =>{
-        if(sum>target) return;
-        if(sum == target){
-            res.push(temp)
+    const backtrack = (nums, index, t) => {
+        if(t<0) return;
+        if(t===0){
+            // anset.add(nums.join(''));
+            ans.push(nums);
             return;
         }
-        // 1 1 2 5 6 7 10 
-        for(let i =index; i<c.length;i++){
-            if(i != index && c[i] == c[i-1]) continue;
-            iterate(i+1,sum+c[i],[...temp,c[i]])
+                
+        for(let i=index;i<candidates.length;i++){
+            if(i!==index && candidates[i-1]===candidates[i]) continue;
+            backtrack([...nums,candidates[i]],i+1,t-candidates[i])
         }
-    }
-    iterate(0,0,[])
-    return res;
+    };
+    
+    backtrack([],0,target);
+    return ans;
 };
