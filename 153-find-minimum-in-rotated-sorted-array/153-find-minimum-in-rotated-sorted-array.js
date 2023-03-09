ff9beb430db389596163beea8c9a56fb360b6b6c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-03-09 08:46:51 -0500

    Time: 77 ms (10.89%), Space: 44.2 MB (5.04%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    while(left < right){
+        let mid = Math.floor((left+right)/2);
+        console.log(nums[left],nums[mid],nums[right]);
+        
+        if(nums[left] < nums[right]) return nums[left];
+        
+        if(nums[left]<=nums[mid]){
+            left = mid+1;
+        } else {
+            right = mid;
+        }
+    }
+    
+    return nums[left];
+};
