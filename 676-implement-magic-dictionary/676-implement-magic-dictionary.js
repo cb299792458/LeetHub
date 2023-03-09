8b9088f381be9dc970bf2eb4520c82398115fcdd
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-03-09 10:45:01 -0500

    Time: 97 ms (67.86%), Space: 49.3 MB (57.14%) - LeetHub

--- 676-implement-magic-dictionary/676-implement-magic-dictionary.js
+++ 676-implement-magic-dictionary/676-implement-magic-dictionary.js
@@ -13,33 +13,41 @@ MagicDictionary.prototype.buildDict = function(dictionary) {
 MagicDictionary.prototype.search = function(searchWord) {
     // if(this.dict.has(searchWord)) return false;
     
-    let rightLength = false;
-    for(let dictWord of this.dict){
-        if(dictWord.length === searchWord.length) rightLength = true;
-    }
-    if(!rightLength) return false;
+//     let rightLength = false;
+//     for(let dictWord of this.dict){
+//         if(dictWord.length === searchWord.length) rightLength = true;
+//     }
+//     if(!rightLength) return false;
     
-    for(let index = 0; index<searchWord.length; index++){
-        let char = searchWord[index];
-        for(let letter of letters){ // for every letter
-            if(char===letter) continue;
+//     for(let index = 0; index<searchWord.length; index++){
+//         let char = searchWord[index];
+//         for(let letter of letters){ // for every letter
+//             if(char===letter) continue;
             
-            let newWord = searchWord.slice(0,index) + letter + searchWord.slice(index+1);
-            if(this.dict.has(newWord)) return true;
-        }
+//             let newWord = searchWord.slice(0,index) + letter + searchWord.slice(index+1);
+//             if(this.dict.has(newWord)) return true;
+//         }
+//     }
+    for(let word of [...this.dict]){
+        if(compare(word,searchWord)===1) return true;
     }
     
     return false;
 };
 
-/*
-searchWords: scar
-dict has scar
-s->letter
-acar, bcar, ccar, scar NOT CHANGED
-
-
-*/
 function compare(str1,str2){
+    if(str1.length !== str2.length) return false;
     
+    let differences = 0;
+    for(let i=0;i<str1.length;i++){
+        if(str1[i]!==str2[i]) differences++;
+    }
+    return differences;
 }
+
+
+
+
+
+
+
