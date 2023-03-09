e0665a347e52ba939e93e8a290461c256d4dbd0d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-03-09 11:36:37 -0500

    Time: 782 ms (5.02%), Space: 95.6 MB (68.57%) - LeetHub

--- 146-lru-cache/146-lru-cache.js
+++ 146-lru-cache/146-lru-cache.js
@@ -1,87 +1,71 @@
-/**
- * @param {number} capacity
- */
-class Node{
-    constructor(key,val,next,prev){
+class Node {
+    constructor(key,val,prev,next){
         this.key = key;
         this.val = val;
-        this.next = next;
-        this.prev = prev;
+        this.prev = null;
+        this.next = null;
     }
 }
 
-var LRUCache = function(capacity) {
-    this.capacity = capacity;
-    this.map = new Map;
-    this.head = new Node();
-    this.tail = new Node();
-    this.head.next = this.tail;
-    this.tail.prev = this.head;
-};
 
-/** 
- * @param {number} key
- * @return {number}
- */
-LRUCache.prototype.get = function(key) {
-    if(!this.map.has(key)) return -1;
-    
-    let node = this.map.get(key);
-    
-    let before = node.prev;
-    let after = node.next;
-    before.next = after;
-    after.prev = before
-    
-    let second = this.head.next;
-    this.head.next = node;
-    node.prev = this.head;
-    node.next = second;
-    second.prev = node;
-
-    return node.val
-};
-
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function(key, value) {
-    let node;
-    if(!this.map.has(key)){
-        node = new Node(key, value);
-        this.map.set(key,node);
-    } else {
-        node = this.map.get(key);
-        node.val = value;
-        this.map.set(key,node);
+class LRUCache {
+    constructor(capacity){
+        this.capacity = capacity;
+        this.hashMap = new Map();
         
+        this.head = new Node('head','head');
+        this.tail = new Node('tail','tail');
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+    
+    delete(node){
         let before = node.prev;
         let after = node.next;
-        before.next = after;
-        after.prev = before
-    }
-    
-    let second = this.head.next;
-    this.head.next = node;
-    node.prev = this.head;
-    node.next = second;
-    second.prev = node;
-    
-    if(this.map.size > this.capacity){
-        let last = this.tail.prev;
-        let nextToLast = last.prev;
         
-        nextToLast.next = this.tail;
-        this.tail.prev = nextToLast;
-        this.map.delete(last.key);
+        before.next = after;
+        after.prev = before;
     }
-};
+    
+    add(node){
+        let second = this.head.next;
+        
+        this.head.next = node;
+        node.prev = this.head;
+        
+        second.prev = node;
+        node.next = second;
+    }
+    
+    
+    put(key,val){
+        if(this.hashMap.has(key)){
+            let node = this.hashMap.get(key);
+            this.delete(node);
+            this.hashMap.delete(key);
+        }
+        let node = new Node(key,val);
+        this.hashMap.set(key,node);
+        this.add(node);
+        this.evict();
+    }
+    
+    get(key){
+        if(!this.hashMap.has(key)) return -1;
+        let node = this.hashMap.get(key);
+        this.delete(node);
+        this.add(node);
+        this.evict();
+        return node.val;
+    }
+    
+    evict(){
+        if(this.hashMap.size > this.capacity){
+            let node = this.tail.prev;
+            console.log('deleting ', node.key,node.val);
+            this.delete(node);
+            this.hashMap.delete(node.key);
+        }
+    }
 
-/** 
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
+}
