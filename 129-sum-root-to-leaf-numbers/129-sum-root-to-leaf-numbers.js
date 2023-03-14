6a1dcd83128c94a57fa330c93e1c29aba6e55870
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-03-14 16:57:07 -0400

    Time: 81 ms (5.05%), Space: 44.6 MB (5.59%) - LeetHub

---
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    function traverse(node, curr = 0){
+        console.log(curr);
+        if(!node) return curr;
+        curr = (10*curr) + node.val;
+        
+        if(!node.right) return traverse(node.left,curr);
+        if(!node.left) return traverse(node.right,curr);
+        return traverse(node.left,curr) + traverse(node.right,curr);
+    }
+    
+    return traverse(root);
+};
