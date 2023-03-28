1d6dad1d2ada8abb5cafc305d86874409dc33c8d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-03-28 18:48:59 -0400

    Time: 186 ms (64.94%), Space: 55.9 MB (92.19%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,29 +3,28 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    const res = [];
-    const set = new Set;
+    let result = [];
+    nums.sort((a,b) => a - b )
     
-    for(let i=0;i<nums.length;i++){
-        const first = nums[i];
-        const map = {};
-        for(let j=i+1;j<nums.length;j++){
-            const second=nums[j];
-            if(map[second]===undefined){
-                map[0-first-second]=second;
-            } else{
-                let ans = [first, second, map[second]];
-                ans = ans.sort((a,b)=>a-b);
-                
-                if(!set.has(`${ans[0]},${ans[1]},${ans[2]}`)){
-                    res.push(ans);
-                    set.add(`${ans[0]},${ans[1]},${ans[2]}`)
-                }
-                
+    for (let i = 0; i<nums.length; i++) {
+        if (i > 0 && nums[i-1] === nums[i]) continue;
+        
+        let j = i + 1
+        let k = nums.length - 1
+        
+        while (j < k) {
+            let currSum = nums[i] + nums[j] + nums[k];
+            
+            if (currSum > 0) {
+                k -= 1
+            } else if (currSum < 0) {
+                j += 1;
+            } else {
+                result.push([nums[i], nums[j], nums[k]])
+                j += 1;
+                while (nums[j] === nums[j-1] && j < k) j++;
             }
         }
     }
-    
-    return res;
-    
+    return result;
 };
