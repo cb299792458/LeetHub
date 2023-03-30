7550fe24e567f4f78375744a38a5d7c2d4306568
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-03-30 14:25:48 -0400

    Time: 59 ms (78.62%), Space: 41.9 MB (94.63%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.js
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -4,21 +4,32 @@
  */
 var generateParenthesis = function(n) {
     const ans = [];
+    
     function backtrack(opens, closes, combo){
         if(opens===0&&closes===n){
             ans.push(combo);
             return;
         }
         if(opens>0){
-            backtrack(opens-1,closes+1,combo+')');
+            backtrack(opens-1,closes+1,combo+')'); // combo = '()''
         }
         if(opens+closes<n){
-            backtrack(opens+1,closes,combo+'(');
+            backtrack(opens+1,closes,combo+'('); // combo = '('
         }
         
     };
     
-    backtrack(0,0,'');
-    return ans;
+    backtrack(0,0,''); // combo = ''
     
+    return ans;
 };
+
+
+/*
+(
+
+()    ((
+
+()()  (())
+
+*/
