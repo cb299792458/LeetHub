6ceaed30956c0509e96ce88d958ca8d28ac4e9b5
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-03-30 13:34:06 -0400

    Time: 95 ms (48.98%), Space: 48.5 MB (74.02%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -6,8 +6,6 @@ var countBits = function(n) {
     memo = [];
     for(let i=0;i<=n;i++){
         let j = i; 
-        
-        
         memo.push(binary(j));
     }
     return memo;
@@ -16,8 +14,6 @@ var countBits = function(n) {
 
 function binary(j){
     let ans = 0;
-    
-
     while (j) {
       if(j%2) ans++
       j = Math.floor(j / 2);
