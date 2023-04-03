e51019f73a2929e88418cf9df8be1225533a814d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-03 13:38:31 -0400

    Time: 127 ms (97.34%), Space: 13.9 MB (87.42%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,27 +1,27 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-#         # PURE DP/MEMO
-#         memo=[0]*(amount+1)
-#         memo[0]=1
+        # PURE DP/MEMO
+        memo=[0]*(amount+1)
+        memo[0]=1
 
-#         for coin in coins:
-#             for i in range(coin,amount+1):
-#                 memo[i] += memo[i-coin]
+        for coin in coins:
+            for i in range(coin,amount+1):
+                memo[i] += memo[i-coin]
         
-#         return memo[amount]
+        return memo[amount]
 
 
-        # DFS
-        memo={}
-        def dfs(amt,idx):
-            if amt<0 or idx==len(coins): return 0 # passed target amt, or last coin
-            if amt==0: return 1 # hit target
+#         # DFS/BACKTRACK
+#         memo={}
+#         def dfs(amt,idx):
+#             if amt<0 or idx==len(coins): return 0 # passed target amt, or last coin
+#             if amt==0: return 1 # hit target
             
-            if (amt,idx) in memo: return memo[(amt,idx)]
+#             if (amt,idx) in memo: return memo[(amt,idx)]
             
-            memo[(amt,idx)] = dfs(amt-coins[idx],idx) + dfs(amt,idx+1) # use current coin, or go to next
-            return memo[(amt,idx)]
+#             memo[(amt,idx)] = dfs(amt-coins[idx],idx) + dfs(amt,idx+1) # use current coin, or go to next
+#             return memo[(amt,idx)]
         
         
-        return dfs(amount,0)
+#         return dfs(amount,0)
