99d372c8660566d34025fa6ce2a4b63eab67481e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-03 13:36:58 -0400

    Time: 929 ms (22.52%), Space: 163.3 MB (12.94%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,10 +1,26 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo=[0]*(amount+1)
-        memo[0]=1
-
-        for coin in coins:
-            for i in range(coin,amount+1):
-                memo[i] += memo[i-coin]
         
-        return memo[amount]
+#         # PURE DP/MEMO
+#         memo=[0]*(amount+1)
+#         memo[0]=1
+
+#         for coin in coins:
+#             for i in range(coin,amount+1):
+#                 memo[i] += memo[i-coin]
+        
+#         return memo[amount]
+        
+        memo={}
+        
+        def dfs(amt,idx):
+            if amt<0 or idx==len(coins): return 0
+            if amt==0: return 1
+            
+            if (amt,idx) in memo: return memo[(amt,idx)]
+            
+            memo[(amt,idx)] = dfs(amt-coins[idx],idx) + dfs(amt,idx+1)
+            return memo[(amt,idx)]
+        
+        
+        return dfs(amount,0)
