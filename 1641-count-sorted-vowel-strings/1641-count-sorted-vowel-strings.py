b9126458212755a481384164e8ab3a8a24c701cb
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-03 13:54:10 -0400

    Time: 33 ms (61.80%), Space: 13.7 MB (94.71%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        vowels = ["a","e","i","o","u"]
+        endings = {'a':1,'e':1,'i':1,'o':1,'u':1}
+        
+        for i in range(2,n+1):
+            temp = {'a':0,'e':0,'i':0,'o':0,'u':0}
+            
+            for end in range(5):
+                for new in range(end,5):
+                    temp[vowels[new]] += endings[vowels[end]]
+                    
+                
+            endings = temp
+            
+        # print(endings)
+        return sum(endings.values())
