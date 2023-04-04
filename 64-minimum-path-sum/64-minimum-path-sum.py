96b483f492e97eeb0a782527c7861a2c4ef75a62
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-04 16:55:21 -0400

    Time: 100 ms (57.44%), Space: 15.7 MB (43.66%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -4,12 +4,10 @@ class Solution:
             subarray.append(float('inf'))
         grid.append([float('inf')]*len(grid[0]))
         
-        
         for row in range(len(grid)-2,-1,-1):
             for col in range(len(grid[0])-2,-1,-1):
                 if row==len(grid)-2 and col==len(grid[0])-2: continue
                 grid[row][col]+=min(grid[row][col+1],grid[row+1][col])
-                
-                
+                     
         # print(grid)
         return grid[0][0]
