3b0d0ca0d7826b64cff3a54a8322b371a0a4cb37
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-06 09:09:51 -0400

    Time: 135 ms (45.64%), Space: 14.6 MB (39.95%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -6,7 +6,6 @@ class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
 
         def fill(r,c):
-            print(f'filling {r} {c}')
             if r<0 or r>=len(grid): return
             if c<0 or c>=len(grid[0]): return
             
@@ -26,7 +25,6 @@ class Solution:
             fill(0,col)
             fill(len(grid)-1,col)
             
-        print(grid)
         islands = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
