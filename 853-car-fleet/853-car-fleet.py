783f0c4dcc2d319407eaba48a7f95f1b5f2c751a
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-08 11:24:10 -0400

    Time: 931 ms (76.56%), Space: 39.5 MB (7.52%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -2,7 +2,6 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         cars = [[position[i],speed[i],(target-position[i])/speed[i]] for i in range(len(position))]
         cars.sort(key=lambda x: x[0], reverse=False)
-        print(cars)
                
         fleets=0
         stack=[cars.pop()]
@@ -14,5 +13,4 @@ class Solution:
             if last_car[2] < new_car[2]:
                 stack.append(new_car)
         
-        print(stack)
         return len(stack)
