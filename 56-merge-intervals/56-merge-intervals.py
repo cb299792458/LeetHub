b912675abc2d7446be10475b98ad9295c3e2bd75
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-10 15:43:44 -0400

    Time: 141 ms (92.50%), Space: 18.1 MB (44.65%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda a: a[0])
+        
+        stack = deque([intervals[0]])
+        
+        # print(stack)
+        for i in range(1,len(intervals)):
+            if stack[-1][1]>=intervals[i][0]:
+                stack[-1] = [stack[-1][0],max(stack[-1][1],intervals[i][1])]
+            else:
+                stack.append(intervals[i])
+        return [*stack]
