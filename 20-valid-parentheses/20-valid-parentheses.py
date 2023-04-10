424ab25ffe45c3e1b693e1bfade7e425c7ef39fd
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-10 15:38:08 -0400

    Time: 31 ms (74.43%), Space: 13.9 MB (58.05%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,7 +5,7 @@ class Solution:
         for c in s:
             if c in dictionary:
                 stack.append(c)
-            elif len(stack)>0 and c == dictionary[stack[-1]]:
+            elif stack and c == dictionary[stack[-1]]:
                 stack.pop()
             else:
                 return False
