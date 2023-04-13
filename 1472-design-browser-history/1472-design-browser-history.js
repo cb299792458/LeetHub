2d093570fa8d29b09928e4aedaf8ab69b7eae28f
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-13 15:02:42 -0400

    Time: 182 ms (35.34%), Space: 52 MB (35.28%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.js
+++ 1472-design-browser-history/1472-design-browser-history.js
@@ -2,17 +2,17 @@
  * @param {string} homepage
  */
 var BrowserHistory = function(homepage) {
-    this.history = [homepage]
-    this.currentPage = 0
+    this.history = [homepage];
+    this.currentPage = 0;
 };
 /** 
  * @param {string} url
  * @return {void}
  */
 BrowserHistory.prototype.visit = function(url) {
-    this.history = this.history.slice(0, this.currentPage+1)
-    this.history.push(url)
-    this.currentPage ++
+    this.history = this.history.slice(0, this.currentPage+1);
+    this.history.push(url);
+    this.currentPage++;
 };
 /** 
  * @param {number} steps
@@ -20,11 +20,11 @@ BrowserHistory.prototype.visit = function(url) {
  */
 BrowserHistory.prototype.back = function(steps) {
     if(this.currentPage >= steps){
-        this.currentPage -= steps
+        this.currentPage -= steps;
     } else {
-        this.currentPage = 0
+        this.currentPage = 0;
     }
-    return this.history[this.currentPage]
+    return this.history[this.currentPage];
     
 };
 /** 
@@ -33,9 +33,9 @@ BrowserHistory.prototype.back = function(steps) {
  */
 BrowserHistory.prototype.forward = function(steps) {
     if(this.history.length > steps + this.currentPage){
-        this.currentPage += steps
+        this.currentPage += steps;
     } else {
-        this.currentPage = this.history.length - 1
+        this.currentPage = this.history.length - 1;
     }
-    return this.history[this.currentPage]
+    return this.history[this.currentPage];
 };
