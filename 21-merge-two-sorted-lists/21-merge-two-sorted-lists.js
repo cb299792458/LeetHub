e329d617b5d0b9cc3fc18c0471c8e288d9d53ddd
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-13 14:20:38 -0400

    Time: 79 ms (15.42%), Space: 44.5 MB (25.33%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,13 +11,37 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(!list1){return list2}
-    if(!list2){return list1}
-    if(list1.val <= list2.val){
-        list1.next = mergeTwoLists(list1.next, list2);
-        return list1;
-    } else {
-        list2.next = mergeTwoLists(list1, list2.next);
-        return list2;
+    // // RECURSIVE node by node
+    // if(!list1){return list2}
+    // if(!list2){return list1}
+    // if(list1.val <= list2.val){
+    //     list1.next = mergeTwoLists(list1.next, list2);
+    //     return list1;
+    // } else {
+    //     list2.next = mergeTwoLists(list1, list2.next);
+    //     return list2;
+    // }
+    
+    // ITERATIVE same as mergesort
+    
+    if(!list1) return list2
+    if(!list2) return list1
+    
+    const dummy = new ListNode()
+    let current = dummy;
+
+    while(list1 && list2){
+        if(list1.val<list2.val){
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
     }
+    
+    current.next = list1 ? list1 : list2;
+    return dummy.next;
+    
 };
