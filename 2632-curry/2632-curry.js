18ada8a76595efdaf8e741cddd1c5ec755fac00a
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-13 19:19:32 -0400

    Time: 64 ms (64.29%), Space: 43.8 MB (89.29%) - LeetHub

---
+++ 2632-curry/2632-curry.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+    let args = [];
+    let num = fn.length;
+    return function curried(...arg) {
+        args.push(...arg);
+        if (args.length === num) return fn(...args)
+        return curried;
+    }
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
