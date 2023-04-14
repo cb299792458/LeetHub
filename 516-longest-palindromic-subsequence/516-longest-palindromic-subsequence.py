9e032b9aea024ff4797fffeaa32cb3e5377fd4c2
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-14 08:50:34 -0400

    Time: 3291 ms (16.48%), Space: 194.7 MB (21.09%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -1,9 +1,7 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         memo = dict()
-        @cache
         def dfs(l,r):
-            # print(str([l,r]))
             if l==r: return 1
             if l>r: return 0
             if str([l,r]) in memo: return memo[str([l,r])]
