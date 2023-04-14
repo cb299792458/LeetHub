2908a95ccf4bfc3e6539ff08427249796327aa71
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-14 08:51:50 -0400

    Time: 1297 ms (77.00%), Space: 249.8 MB (15.82%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -4,12 +4,12 @@ class Solution:
         def dfs(l,r):
             if l==r: return 1
             if l>r: return 0
-            if str([l,r]) in memo: return memo[str([l,r])]
+            if (l,r) in memo: return memo[(l,r)]
             
             if s[l]==s[r]:
-                memo[str([l,r])]=2+dfs(l+1,r-1)
+                memo[(l,r)]=2+dfs(l+1,r-1)
             else:
-                memo[str([l,r])]=max(dfs(l+1,r),dfs(l,r-1))
-            return memo[str([l,r])]
+                memo[(l,r)]=max(dfs(l+1,r),dfs(l,r-1))
+            return memo[(l,r)]
             
         return dfs(0,len(s)-1)
