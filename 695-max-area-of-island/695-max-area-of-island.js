78aec044a238ddcbc4e1d6e3c297af182f51b44d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-19 12:56:04 -0400

    Time: 100 ms (23.29%), Space: 49.4 MB (19.01%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -3,36 +3,36 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    let set = new Set()
-    let max = 0
+    let set = new Set();
+    let max = 0;
     
     
     for(let r = 0; r<grid.length;r++){
         for(let c = 0; c < grid[0].length; c++){
-            max = Math.max(max,dfs(grid,r,c,set))
+            max = Math.max(max,dfs(grid,r,c,set));
         }
     }
     
-    return max
+    return max;
     
 };
 
 function dfs(grid,row,col,visited){
     if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited.has(`${row},${col}`) || grid[row][col] === 0){
-        return 0
+        return 0;
     }
     
-    visited.add(`${row},${col}`)
+    visited.add(`${row},${col}`);
     
-    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+    const dirs = [[1,0],[-1,0],[0,1],[0,-1]];
     
-    let count = 1
+    let count = 1;
     
     for(let dir of dirs){
-        const newRow = row + dir[0]
-        const newCol = col + dir[1]
-        count += dfs(grid,newRow,newCol,visited)
+        const newRow = row + dir[0];
+        const newCol = col + dir[1];
+        count += dfs(grid,newRow,newCol,visited);
     }
     
-    return count
+    return count;
 }
