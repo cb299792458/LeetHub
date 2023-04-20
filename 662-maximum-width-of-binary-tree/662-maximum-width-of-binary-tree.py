e16db6425384a6975069cb38b7e8df9c8432eaf6
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-20 10:12:54 -0400

    Time: 53 ms (23.85%), Space: 18.3 MB (21.33%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        maxs=[]
+        mins=[]
+        
+        def _traverse(node,index,level):
+            if not node: return
+            
+            if level >= len(maxs):
+                maxs.append(index)
+                mins.append(index)
+            else:
+                maxs[level]=max(maxs[level],index)
+                mins[level]=min(mins[level],index)
+            
+            _traverse(node.left,2*index,level+1)
+            _traverse(node.right,2*index+1,level+1)
+            
+        _traverse(root,0,0)
+        # print(maxs,mins)
+        diffs = [maxs[i]-mins[i] for i in range(len(maxs))]
+        return max(diffs)+1
+        
