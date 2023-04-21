0e39d4a7e175f10d7ed032c7d9135ecbda991b44
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-04-21 12:37:47 -0400

    Time: 207 ms (24.91%), Space: 33.4 MB (7.01%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -0,0 +1,40 @@
+class Trie:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        
+        for char in word:
+            i = ord(char)-ord('a')
+            if current.children[i] == None: current.children[i] = Node()
+            current = current.children[i]
+        
+        current.word = True
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        
+        for char in word:
+            i = ord(char)-ord('a')
+            if not current.children[i]: return False
+            current = current.children[i]
+
+        return current.word
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        
+        for char in prefix:
+            i = ord(char)-ord('a')
+            if not current.children[i]: return False
+            current = current.children[i]
+
+        return current != None
+        
+
+class Node:
+    def __init__(self):
+        self.word = False
+        self.children = [None]*26
