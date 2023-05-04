30037e62ae4cddb956c52f1e8c1c81c88eac89ca
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-05-04 09:35:28 -0400

    Time: 80 ms (38.84%), Space: 16.5 MB (11.62%) - LeetHub

---
+++ 649-dota2-senate/649-dota2-senate.py
@@ -0,0 +1,26 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        r=0
+        d=0
+        senate = [*senate]
+        no_r=False
+        no_d=False
+        while True:
+            no_r=no_d=True
+            for i in range(len(senate)):
+                if senate[i]=='R':
+                    no_r=False
+                    if d:
+                        d-=1
+                        senate[i]='X'
+                    else: r+=1
+                elif senate[i]=='D':
+                    no_d=False
+                    if r:
+                        r-=1
+                        senate[i]='X'
+                    else: d+=1
+            if no_r: return "Dire"
+            if no_d: return "Radiant"
+                        
+                        
