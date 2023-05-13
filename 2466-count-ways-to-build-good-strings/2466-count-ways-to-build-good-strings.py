4df3e3e4025ba16c8c0c85139100ad1aef970a6e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-05-13 19:46:01 -0400

    Time: 337 ms (52.24%), Space: 32.7 MB (62.69%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -5,12 +5,12 @@ class Solution:
         dp[0]=1
         for i in range(1,high+1):
             if i>=zero:
-                dp[i]+=dp[i-zero]
+                dp[i] = (dp[i-zero]+dp[i]) % (10**9+7)
             if i>=one:
-                dp[i]+=dp[i-one]
+                dp[i] = (dp[i-one]+dp[i]) % (10**9+7)
             
         # print(dp)
         sum=0
         for i in range(low,high+1):
-            sum+=dp[i]
-        return sum % (10**9+7)
+            sum = (sum + dp[i]) % (10**9+7)
+        return sum
