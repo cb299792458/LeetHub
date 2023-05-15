cbddda5cd15c1acbca84844db78b4f361b814a4f
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-05-15 13:57:51 -0400

    Time: 1224 ms (9.54%), Space: 30.8 MB (33.98%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,17 +1,45 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        res=0
-        stack=[]
+#         res=0
+#         stack=[]
         
-        heights = heights + [0]
+#         heights = heights + [0]
+#         for i in range(len(heights)):
+#             w=0
+#             while stack and stack[-1]>heights[i]:
+#                 w+=1
+#                 h=stack.pop()
+#                 res = max(res,w*h)
+                
+#             stack += [heights[i]] * (w+1)
+                
+                
+#         return res
+        res=0
+        heights.append(0)
+        next_smaller = [-1 for _ in range(len(heights))]
+        prev_smaller = [-1 for _ in range(len(heights))]
+        
+        stack=[]
         for i in range(len(heights)):
-            w=0
-            while stack and stack[-1]>heights[i]:
-                w+=1
-                h=stack.pop()
-                res = max(res,w*h)
-                
-            stack += [heights[i]] * (w+1)
-                
-                
+            while stack and heights[stack[-1]]>=heights[i]:
+                stack.pop()
+            prev_smaller[i] = stack[-1] if stack else -1
+            stack.append(i)
+            
+        stack=[]
+        for i in range(len(heights)-1,-1,-1):
+            while stack and heights[stack[-1]]>=heights[i]:
+                stack.pop()
+            next_smaller[i] = stack[-1] if stack else -1
+            stack.append(i)
+            
+        print(prev_smaller)
+        print(next_smaller)
+        for i in range(len(heights)):
+            height = heights[i]
+            width = next_smaller[i] - prev_smaller[i] - 1
+            res = max(res,height*width)
+        
         return res
+        
