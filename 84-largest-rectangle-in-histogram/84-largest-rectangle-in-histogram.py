9d98d1f2a50907a00cafff1a06b3d54bad116d36
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-05-15 14:10:47 -0400

    Time: 1367 ms (5.02%), Space: 31.7 MB (19.48%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,44 +1,47 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-#         res=0
-#         stack=[]
-        
-#         heights = heights + [0]
-#         for i in range(len(heights)):
-#             w=0
-#             while stack and stack[-1]>heights[i]:
-#                 w+=1
-#                 h=stack.pop()
-#                 res = max(res,w*h)
-                
-#             stack += [heights[i]] * (w+1)
-                
-                
-#         return res
         res=0
-        heights.append(0)
-        next_smaller = [-1 for _ in range(len(heights))]
-        prev_smaller = [-1 for _ in range(len(heights))]
-        
         stack=[]
-        for i in range(len(heights)):
-            while stack and heights[stack[-1]]>=heights[i]:
-                stack.pop()
-            prev_smaller[i] = stack[-1] if stack else -1
-            stack.append(i)
-            
-        stack=[]
-        for i in range(len(heights)-1,-1,-1):
-            while stack and heights[stack[-1]]>=heights[i]:
-                stack.pop()
-            next_smaller[i] = stack[-1] if stack else -1
-            stack.append(i)
-            
-        for i in range(len(heights)):
-            height = heights[i]
-            width = next_smaller[i] - prev_smaller[i] - 1
-            # print(next_smaller[i],prev_smaller[i],width)
-            res = max(res,height*width)
         
+        heights = heights + [0]
+        for i in range(len(heights)):
+            w=0
+            while stack and stack[-1]>heights[i]:
+                w+=1
+                h=stack.pop()
+                res = max(res,w*h)
+                
+            stack += [heights[i]] * (w+1)
+                
+                
         return res
+
+
+#         # three passes, find indices of lower heights to left and right
+#         res=0
+#         heights.append(0)
+#         next_smaller = [-1 for _ in range(len(heights))]
+#         prev_smaller = [-1 for _ in range(len(heights))]
+        
+#         stack=[]
+#         for i in range(len(heights)):
+#             while stack and heights[stack[-1]]>=heights[i]:
+#                 stack.pop()
+#             prev_smaller[i] = stack[-1] if stack else -1
+#             stack.append(i)
+            
+#         stack=[]
+#         for i in range(len(heights)-1,-1,-1):
+#             while stack and heights[stack[-1]]>=heights[i]:
+#                 stack.pop()
+#             next_smaller[i] = stack[-1] if stack else -1
+#             stack.append(i)
+            
+#         for i in range(len(heights)):
+#             height = heights[i]
+#             width = next_smaller[i] - prev_smaller[i] - 1
+#             # print(next_smaller[i],prev_smaller[i],width)
+#             res = max(res,height*width)
+        
+#         return res
         
