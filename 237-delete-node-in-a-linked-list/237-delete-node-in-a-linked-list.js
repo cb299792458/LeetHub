60b019acac363d0fe1d7d59c16ae04530f8ba534
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-05-23 12:36:31 -0400

    Time: 67 ms (60.34%), Space: 44.1 MB (75.17%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
@@ -1,27 +1,5 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} node
- * @return {void} Do not return anything, modify node in-place instead.
- */
+
 var deleteNode = function(node) {
     node.val = node.next.val;
     node.next = node.next.next;
 };
-
-/*
-
-[4,5,9]
- 4,1,9,X
- 4,1,9
- 
- 
- 
-5
-
-*/
