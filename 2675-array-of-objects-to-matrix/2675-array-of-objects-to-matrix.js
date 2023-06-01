00a3aab1db2c8923b8ebf6fc5ac1160a3c7662e3
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-06-01 09:08:01 -0400

    Time: 507 ms (28.81%), Space: 93 MB (62.75%) - LeetHub

--- 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -1,7 +1,3 @@
-/**
- * @param {Array} arr
- * @return {Matrix}
- */
 var jsonToMatrix = function(arr) {
     let keys = new Set();
     
@@ -21,7 +17,7 @@ var jsonToMatrix = function(arr) {
     keys.sort( (a,b) => a<b ? -1 : 1 ); // default sort works fine
     const res = new Array(arr.length+1).fill().map(()=>new Array(keys.length).fill('')); 
     res[0] = keys;
-    console.log(res[0]) // now we can finally do the problem
+    // console.log(res[0]) // now we can finally do the problem
     
     for(let r=1;r<res.length;r++){ // skip row 0, which is all keys
         let line = arr[r-1]; // need a -1 !!! b/c arr has one less row than res !!!
@@ -35,7 +31,6 @@ var jsonToMatrix = function(arr) {
                 /*
                 This hack is for when a path can lead into an array or a string. If it hits an array, the next key might be an index, but when the same path is checked and it points to a string, it will get that character in the string, but it should just continue, i.e., element not found.
                 */
-                
                 
                 for(let step of steps){
                     if(typeof current==='string') hack=true;
