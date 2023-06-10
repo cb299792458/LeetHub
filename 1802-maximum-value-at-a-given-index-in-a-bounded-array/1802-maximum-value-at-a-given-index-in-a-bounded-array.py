31d97ab7005e023c7d554b253806f675dfab4efd
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-06-10 14:07:35 -0400

    Time: 55 ms (38.17%), Space: 16.4 MB (6.11%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxValue(self, n: int, index: int, maxSum: int) -> int:
+        def check(target):
+            minSum = target
+            
+            # left side
+            minSum+=(target*(target-1))//2
+            if target<index+1:
+                minSum+=index+1-target
+            else:
+                minSum-=(target-(index+1))*(target-(index+1)+1)//2
+            
+            # right side
+            minSum+=(target*(target-1))//2
+            spaces = n-index-1
+            if target-1<spaces:
+                minSum+=spaces-target+1
+            else:
+                minSum-=(target-spaces-1)*(target-spaces)//2
+                
+            print(target,minSum)
+            return minSum
+        
+        # check(271698267)
+        
+        l,r = 1,maxSum
+        while l<r:
+            m = (r+l+1)//2
+            if check(m)<=maxSum:
+                l = m
+            else: r = m-1
+        return l
