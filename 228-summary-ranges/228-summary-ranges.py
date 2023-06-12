58bd0a5f7651ae3c76c1b1f0dd1b152c1637d609
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-06-12 09:01:52 -0400

    Time: 51 ms (21.61%), Space: 16.2 MB (81.48%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -6,8 +6,7 @@ class Solution:
         start=prev=nums[0]
         nums.append(float('inf'))
         
-        for i,n in enumerate(nums):
-            if not i: continue
+        for n in nums[1:]:
             if n==prev+1:
                 prev+=1
             else:
