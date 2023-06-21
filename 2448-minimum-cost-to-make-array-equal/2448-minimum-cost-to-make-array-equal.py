8694e5725705d0a2599fe34c879e05d812eb2346
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-06-21 09:19:12 -0400

    Time: 947 ms (36.76%), Space: 31.9 MB (52.94%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minCost(self, nums: List[int], costs: List[int]) -> int:
+        def find_cost(target):
+            res=0
+            for num,cost in zip(nums,costs):
+                res+=abs(num-target)*cost
+            return res
+        
+        # # TLE
+        # res=float('inf')
+        # for n in nums:
+        #     res=min(res,find_cost(n))
+        # return res
+        
+        l,r = min(nums),max(nums)
+        
+        while l<r:
+            m = l + (r-l)//2
+            if find_cost(m)<find_cost(m+1):
+                r=m
+            else: l=m+1
+        
+        return find_cost(l)
+        
