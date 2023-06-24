89066f14d00f71c9ec005afdf121a0b478268ee7
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-06-24 14:31:55 -0400

    Time: 1711 ms (8.33%), Space: 235 MB (14.58%) - LeetHub

--- 956-tallest-billboard/956-tallest-billboard.py
+++ 956-tallest-billboard/956-tallest-billboard.py
@@ -1,13 +1,19 @@
 class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
-        @lru_cache(None)
+        memo = {}
+        
         def dfs(diff,i):
+            if (diff,i) in memo: return memo[(diff,i)]
+            
             if i==len(rods):
                 if diff: return -float('inf')
                 else: return 0
                 
             r=rods[i]
             i+=1
-            return max(dfs(diff+r,i)+r,dfs(diff-r,i),dfs(diff,i))
+            
+            res = max(dfs(diff+r,i)+r,dfs(diff-r,i),dfs(diff,i))
+            memo[(diff,i-1)]=res
+            return res
     
         return dfs(0,0)
