a915541e510b869010f66bc29535470d5324109b
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-06-29 09:37:57 -0400

    Time: 35 ms (98.90%), Space: 16.4 MB (27.40%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -5,7 +5,7 @@ class Solution:
             digits[i]=0
             i-=1
         if i==-1:
-            digits=[1]+digits
+            return [1]+digits
         else:
             digits[i]+=1
-        return digits
+            return digits
