311e98330dcfcea77771569a456ae54d37aec3f9
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-07-01 20:05:55 -0400

    Time: 50 ms (95.22%), Space: 16.2 MB (89.71%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -3,6 +3,7 @@ class Solution:
         
         ans = float('inf')
         distributions=[0]*k
+        avg = sum(cookies)//k
         
         def backtrack(cookie,distributions):
             if cookie==len(cookies):
@@ -11,6 +12,7 @@ class Solution:
                 return
             
             for child in range(k):
+                if distributions[child]>avg: continue
                 distributions[child]+=cookies[cookie]
                 backtrack(cookie+1,distributions)
                 distributions[child]-=cookies[cookie]
