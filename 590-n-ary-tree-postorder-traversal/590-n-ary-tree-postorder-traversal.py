74328ae14d205bc9caf578b68980d3eae7b86f04
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-07-19 18:53:43 -0400

    Time: 64 ms (66.38%), Space: 18.6 MB (55.09%) - LeetHub

---
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res=[]
+        
+        def helper(node):
+            if not node: return
+            for child in node.children:
+                helper(child)
+            res.append(node.val)
+        
+        helper(root)
+        return res
