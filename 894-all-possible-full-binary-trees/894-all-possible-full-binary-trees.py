20899084698d7288f1214ccbab01cbeebc83ed45
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-07-23 17:33:41 -0400

    Time: 195 ms (40.16%), Space: 20.1 MB (70.37%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.py
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.py
@@ -8,9 +8,9 @@ class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
         dp=defaultdict(list)
         dp[1]=[TreeNode(0)]
-        dp[3]=[TreeNode(0,TreeNode(0),TreeNode(0))]
+        # dp[3]=[TreeNode(0,TreeNode(0),TreeNode(0))]
         
-        for i in range(5,n+2,2):
+        for i in range(3,n+2,2):
             for l in range(1,i,2):
                 r=i-l-1
                 for left in dp[l]:
