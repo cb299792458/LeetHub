2ad35a9b08629fe3c41489e32696c944c3282ffc
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-07-27 09:57:46 -0400

    Time: 1856 ms (46.99%), Space: 31.2 MB (7.23%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        batteries.sort(reverse=True)
+        
+        def check(time):
+            # print(time)
+            extra = sum([min(bat,time) for bat in batteries])
+
+            return extra//n>=time
+        
+        l,r = 1,sum(batteries)//n
+        while l<r:
+            m=r-(r-l)//2
+            if check(m):
+                l=m
+            else:
+                r=m-1
+        return l
