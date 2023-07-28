b7c3c03ba46fcf24e864fbab92068a10a80c3cc4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-07-28 11:36:30 -0400

    Time: 2830 ms (25.36%), Space: 16.2 MB (97.57%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,11 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        def score(l,r):
+            if l==r:
+                return nums[l]
+            take_l = nums[l]-score(l+1,r)
+            take_r = nums[r]-score(l,r-1)
+            
+            return max(take_l,take_r)
+        
+        return score(0,len(nums)-1)>=0
