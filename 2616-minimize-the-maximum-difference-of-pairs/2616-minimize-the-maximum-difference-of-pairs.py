6497b1af9ddda6732bdc531b928db4b0631c0552
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-08-09 14:15:42 -0400

    Time: 815 ms (97.42%), Space: 30.4 MB (100.00%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
@@ -1,27 +1,23 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         nums.sort()
-        n = len(nums)
         
-        # Find the number of valid pairs by greedy approach
-        def countValidPairs(threshold):
-            index, count = 0, 0
-            while index < n - 1:
-                # If a valid pair is found, skip both numbers.
-                if nums[index + 1] - nums[index] <= threshold:
-                    count += 1
-                    index += 1
-                index += 1
-            return count
+        def check(diff):
+            pairs=0
+            copy=nums.copy()
+            while copy:
+                curr=copy.pop()
+                if copy and curr-copy[-1]<=diff:
+                    pairs+=1
+                    copy.pop()
+            return pairs>=p
         
-        left, right = 0, nums[-1] - nums[0]
-        while left < right:
-            mid = left + (right - left) // 2
-
-            # If there are enough pairs, look for a smaller threshold.
-            # Otherwise, look for a larger threshold.
-            if countValidPairs(mid) >= p:
-                right = mid
+        l,r=0,nums[-1]-nums[0]
+        while l<r:
+            m=l+(r-l)//2
+            
+            if check(m):
+                r=m
             else:
-                left = mid + 1
-        return left       
+                l=m+1
+        return l
