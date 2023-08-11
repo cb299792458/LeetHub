6e91cb59d6425a724fd52c0106df2161f6c8cfb5
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-08-11 09:24:55 -0400

    Time: 112 ms (97.84%), Space: 16.4 MB (89.60%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        # TABLE
+        memo=[0]*(amount+1)
+        memo[0]=1
+
+        for coin in coins:
+            for i in range(coin,amount+1):
+                memo[i] += memo[i-coin]
+        
+        return memo[amount]
+
+
+#         # MEMO
+#         memo={}
+#         def dfs(amt,idx):
+#             if amt<0 or idx==len(coins): return 0 # passed target amt, or last coin
+#             if amt==0: return 1 # hit target
+            
+#             if (amt,idx) in memo: return memo[(amt,idx)]
+            
+#             memo[(amt,idx)] = dfs(amt-coins[idx],idx) + dfs(amt,idx+1) # use current coin, or go to next
+#             return memo[(amt,idx)]
+        
+        
+#         return dfs(amount,0)
