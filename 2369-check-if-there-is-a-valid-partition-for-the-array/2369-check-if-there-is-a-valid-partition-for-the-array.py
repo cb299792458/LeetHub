9f99e71087330c6538e3f000d06a1f89f47d0ce4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-08-13 10:54:19 -0400

    Time: 827 ms (96.92%), Space: 30.3 MB (78.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,17 +1,12 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        n = len(nums)
-        dp = [True] + [False] * n
-
-        # Determine if the prefix array nums[0 ~ i] has a valid partition
-        for i in range(n):
-            dp_index = i + 1
-
-            # Check 3 possibilities
-            if i > 0 and nums[i] == nums[i - 1]:
-                dp[dp_index] |= dp[dp_index - 2]
-            if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
-                dp[dp_index] |= dp[dp_index - 3]
-            if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
-                dp[dp_index] |= dp[dp_index - 3]
-        return dp[n]
+        dp = [False]*len(nums)
+        dp[1]=nums[0]==nums[1]
+        if len(nums)==2: return dp[1]
+        dp[2] = nums[0]==nums[1]==nums[2] or nums[0]+2==nums[1]+1==nums[2]
+        
+        for i in range(3,len(nums)):
+            if nums[i]==nums[i-1] and dp[i-2]: dp[i]=True
+            if nums[i]==nums[i-1]==nums[i-2] and dp[i-3]: dp[i]=True
+            if nums[i]==nums[i-1]+1==nums[i-2]+2 and dp[i-3]: dp[i]=True
+        return dp[-1]
