d612c7759b5ea5979afcc89b30c71836622b5e0c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-08-29 15:30:26 -0400

    Time: 279 ms (19.58%), Space: 22.9 MB (14.16%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,31 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n=len(customers)
+        o_pen=[]
+        c_pen=[]
+        
+        n_so_far = 0
+        y_to_come = len([c for c in customers if c=='Y'])
+        
+        for c in customers:
+            o_pen.append(n_so_far)
+            c_pen.append(y_to_come)
+            
+            if c=='Y':
+                y_to_come-=1
+            else:
+                n_so_far+=1
+        
+        o_pen.append(n_so_far)
+        c_pen.append(y_to_come)
+        # print(o_pen,c_pen)
+        
+        best = None
+        lowest = float('inf')
+
+        for (i,(o,c)) in enumerate(zip(o_pen,c_pen)):
+            if o+c<lowest:
+                best = i
+            lowest = min(o+c,lowest)
+            
+        return best
