ea0e30495165a1c70f38a56e5ac9501bee75df4e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-09-02 18:49:50 -0400

    Time: 40 ms (96.94%), Space: 42 MB (61.22%) - LeetHub

---
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var findSubarrays = function(nums) {
+    const seen = new Set();
+    for(let i=0;i<nums.length-1;i++){
+        const total = nums[i]+nums[i+1];
+        if(seen.has(total)){
+            return true;
+        } else {
+            seen.add(total);
+        }
+    }
+    return false;
+};
