6c2546d40ced66c1504242c3dce9b9acd52adc5d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-09-03 12:12:39 -0400

    Time: 55 ms (56.29%), Space: 41.4 MB (95.97%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,7 @@
+var uniquePaths = function(m, n) {
+    let factorials = [1];
+    for(let i = 1; i <= m+n-2; i++){
+        factorials[i] = factorials[i-1] * i;
+    }
+    return (factorials[m+n-2])/(factorials[m-1]*factorials[n-1]);
+};
