ff95fb7d2b90cc52f88057bad1c6e78a64e9ceb6
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-09-06 09:31:26 -0400

    Time: 90 ms (63.83%), Space: 46.7 MB (94.71%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,22 @@
+var addTwoNumbers = function(l1, l2) {
+    
+    let dummy = new ListNode();
+    let current = dummy;
+    let carry = 0;
+    let digit = 0;
+    while(l1 || l2 || carry){
+        digit = 0;
+        if(l1) digit += l1.val;
+        if(l2) digit += l2.val;
+        digit += carry;
+
+        current.next = new ListNode(digit % 10);
+        carry = Math.floor(digit/10);
+        current = current.next;
+        
+        if(l1) l1 = l1.next;
+        if(l2) l2 = l2.next;
+        
+    }
+    return dummy.next;
+};
