006946b8de7bd084a7c63726db18e6847c921bbc
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-09-09 12:12:30 -0400

    Time: 57 ms (83.54%), Space: 46.8 MB (27.07%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -0,0 +1,20 @@
+var combinationSum2 = function(candidates, target) {
+    const ans = [];
+    candidates.sort();
+    
+    const backtrack = (nums, index, t) => {
+        if(t<0) return;
+        if(t===0){
+            ans.push(nums);
+            return;
+        }
+                
+        for(let i=index;i<candidates.length;i++){
+            if(i!==index && candidates[i-1]===candidates[i]) continue;
+            backtrack([...nums,candidates[i]],i+1,t-candidates[i])
+        }
+    };
+    
+    backtrack([],0,target);
+    return ans;
+};
