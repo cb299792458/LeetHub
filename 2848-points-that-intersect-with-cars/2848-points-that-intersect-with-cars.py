2329d1d2b16b4dcd2f5af16e8438dfdb8d311893
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-09-11 13:04:02 -0400

    Time: 84 ms (100.00%), Space: 16.4 MB (100.00%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -1,7 +1,13 @@
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
-        covered_points = []
+        nums.sort()
+        points = 0
+        prev_end = -float('inf')
+        
         for start,end in nums:
-            covered_points.extend(range(start,end+1))
-        unique_points = set(covered_points)
-        return len(unique_points)
+            increase = end + 1 - max(start,prev_end)
+            points += increase if increase > 0 else 0
+            
+            prev_end = max(prev_end,end+1)
+            
+        return points
