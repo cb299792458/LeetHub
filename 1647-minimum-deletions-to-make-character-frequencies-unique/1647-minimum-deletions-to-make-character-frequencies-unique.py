9bfff249083e2655e0f158fd0dc6f0d5f9bed5c0
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-09-11 20:42:03 -0400

    Time: 112 ms (83.78%), Space: 17.3 MB (10.21%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        counts = Counter(s)
+        letters = list(counts.keys())
+        letters.sort(key=lambda k:counts[k],reverse=True)
+        
+        seen = set()
+        deletions = 0
+        
+        for l in letters:
+            while counts[l] and counts[l] in seen:
+                counts[l]-=1
+                deletions+=1
+            seen.add(counts[l])
+            
+        return deletions
