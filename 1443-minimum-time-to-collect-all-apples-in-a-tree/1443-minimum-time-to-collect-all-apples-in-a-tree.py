209be61c55fc17eb487f8309f00f418ba3541804
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-09-29 16:01:26 -0400

    Time: 611 ms (27.33%), Space: 59.2 MB (19.41%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,5 +1,7 @@
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        if not len([bool for bool in hasApple if bool]): return 0
+        
         graph = defaultdict(list)
         for [a,b] in edges:
             graph[a].append(b)
@@ -19,7 +21,6 @@ class Solution:
             if res:
                 must.add(node)
             return res
-        
         dfs(0)
         
-        return max((len(must)-1)*2,0)
+        return (len(must)-1)*2
