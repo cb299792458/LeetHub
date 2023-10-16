14584d8f802aa06f80fa59e4e51284574a67c0ea
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-10-16 09:23:14 -0400

    Time: 42 ms (29.67%), Space: 16.2 MB (53.14%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,13 +1,10 @@
 class Solution:
     def getRow(self, numRows: int) -> List[int]:
-        # copy pasted from Pascal's Triangle I
-        # not optimized, could save space by only keeping last row
-        triangle = [[1]]
+        prev = [1]
         for _ in range(numRows):
             newRow = []
-            prev = triangle[-1]
             for i in range(len(prev)-1):
                 newRow.append(prev[i]+prev[i+1])
-            triangle.append([1]+newRow+[1])
-        return triangle[-1]
+            prev = [1]+newRow+[1]
+        return prev
         
