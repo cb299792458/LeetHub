1776ad27d1f37e726e105a5c6736ebcb684e7561
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-10-17 09:53:32 -0400

    Time: 322 ms (16.77%), Space: 28.5 MB (9.28%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        parents = defaultdict(list)
+        
+        for [i,child] in enumerate(leftChild):
+            parents[child].append(i)
+        for [i,child] in enumerate(rightChild):
+            parents[child].append(i)
+            
+        root=None
+        for i in range(n):
+            if len(parents[i])>1: return False
+            if len(parents[i])==0:
+                if root!=None:
+                    return False
+                else:
+                    root = i
+        if root==None: return False
+        
+        print(parents,root)
+        
+        seen=set()
+        def dfs(node):
+            if node in seen: return
+            seen.add(node)
+            if leftChild[node]!=-1: dfs(leftChild[node])
+            if rightChild[node]!=-1: dfs(rightChild[node])
+        dfs(root)
+        print(seen)
+        return len(seen)==n
+        
