0a062044ec9e837e0ffda1fb19cb72b9be95c18d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-10-30 10:56:52 -0400

    Time: 77 ms (29.96%), Space: 20.6 MB (15.94%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        @cache
+        def bitCount(n):
+            if n==0: return 0
+            return n%2 + bitCount(n//2)
+        
+        arr.sort(key=lambda n: (bitCount(n), n))
+        return arr
