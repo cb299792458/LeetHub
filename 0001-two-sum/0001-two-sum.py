d1398b0c3dbe5c1381d2d545c5b364fb6c34b13f
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-11-10 16:03:23 -0500

    Time: 59 ms (86.29%), Space: 17.5 MB (36.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        copy = sorted(nums)
-        left,right = 0,len(nums)-1
-        while left<right:
-            
-            if copy[left] + copy[right] == target:
-                break
-            elif copy[left] + copy[right] > target:
-                right -= 1
-            else:
-                left += 1
-        first = nums.index(copy[left])
-        second = nums.index(copy[right])
-        if(first==second):
-            second = nums.index(copy[right],first+1)
-        return [first,second]
+        index_of_num = dict()
+        for i,num in enumerate(nums):
+            if target - num in index_of_num:
+                return [i,index_of_num[target - num]]
+            index_of_num[num] = i
+        
