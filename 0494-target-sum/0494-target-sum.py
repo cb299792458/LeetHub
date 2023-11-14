46ce5b25ddb4d978b212b5eb323318dce4972adc
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-11-14 14:27:03 -0500

    Time: 172 ms (81.64%), Space: 16.9 MB (68.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -5,9 +5,8 @@ class Solution:
         # memo[i][total] is the count of ways to reach a total after assigning i symbols
         memo[0][0] = 1
 
-        for i in range(n):
+        for i, num in enumerate(nums):
             prev_dict = memo[i]
-            num = nums[i]
             for [prev_total, count] in prev_dict.items():
                 # for every way to get prev_total, there are that many ways to get to
                 # prev_total+num next round, because you can add num to prev_total 
