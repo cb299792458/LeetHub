1f4381aa98fd24476a284817094dadc1911bcebc
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-11-15 14:34:31 -0500

    Time: 173 ms (81.38%), Space: 16.7 MB (70.03%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,18 +1,33 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        N = len(nums)
-        
+        N = len(nums) # 5
+        # A 2D memo...
+        # memo[index][current] = ways
         memo = [defaultdict(int) for _ in range(N+1)]
-        # memo[nums_passed][current_total] = ways
         memo[0][0] = 1
+        """
+        defaultdict(callback)
+        a dict(), or hashmap, with the return of callback as its default value
         
+        let myObj = {}
+        myObj['key'] ++
+        doesn't work in JS, but you could...
         
-        for i in range(N):
-            num = nums[i]
-            last_col_memo = memo[i]
-            for [current_total, ways] in last_col_memo.items():
-            
-                memo[i+1][current_total+num] += ways
-                memo[i+1][current_total-num] += ways
+        if (!('key' in myDict)) {
+            myObj['key'] = default
+        } \\ or...
+        
+        myObj['key'] ||= default
+        """
+        
+        # index = index in nums, also count of numbers that have been passed so far
+        # current = current sum of all numbers passed so far
+        # ways = number of paths to reach current after index numbers
+        
+        for index in range(N):
+            num = nums[index]
+            for [current,ways] in memo[index].items():
+                memo[index+1][current+num] += ways 
+                memo[index+1][current-num] += ways 
         
         return memo[N][target]
