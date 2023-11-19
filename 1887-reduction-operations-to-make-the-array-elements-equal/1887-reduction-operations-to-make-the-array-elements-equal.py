5a02be11e6e0322a3e78e5f4c52c7cd9057c13bf
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-11-19 09:39:22 -0500

    Time: 836 ms (60.82%), Space: 23.9 MB (45.88%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -3,7 +3,7 @@ class Solution:
         nums.sort(reverse=True)
         ops = 0
         
-        for i,num in enumerate(nums):
-            if i<len(nums)-1 and num>nums[i+1]:
+        for i,num in enumerate(nums[:-1]):
+            if num>nums[i+1]:
                 ops += i+1
         return ops
