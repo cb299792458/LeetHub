5a97b3e36493ae8217b1fed2d805ae23aa72a7a4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-11-24 21:31:59 -0500

    Time: 717 ms (71.80%), Space: 31.9 MB (30.45%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,32 +1,26 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        N=len(nums)
-        prefix=[]
-        suffix=[]
-        l,r=0,0
-        
-        for i in range(N):
-            l+=nums[i]
-            prefix.append(l)
-            
-            r+=nums[-1-i]
-            suffix.append(r)
-            
-        suffix=suffix[::-1]
+        N = len(nums)
         result = []
+        total = sum(nums)
         
-        for i in range(N):
-            n=nums[i]
-            curr=0
+        i = 0
+        passed = 0
+        coming = total
+        
+        for n in nums:
+            curr = 0
             
-            # diffs on left
-            curr+=i*n
-            if i>0: curr-=prefix[i-1]
+            curr += i*n
+            curr -= (N-1-i)*n
+            i += 1
             
-            # diffs on right
-            curr-=(N-1-i)*n
-            if i<N-1: curr+=suffix[i+1]
-                
+            curr -= passed
+            
+            passed += n
+            coming -= n
+            curr += coming
+        
             result.append(curr)
             
         return result
