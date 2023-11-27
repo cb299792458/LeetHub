9cf493527dfd9a8f466f5aad3bb4c49307a08398
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-11-27 08:50:42 -0500

    Time: 844 ms (68.68%), Space: 16.6 MB (67.69%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,25 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        adjs=[
+            [4,6],
+            [6,8],
+            [7,9],
+            [4,8],
+            [3,9,0],
+            [],
+            [1,7,0],
+            [2,6],
+            [1,3],
+            [2,4],
+        ]
+        mod=10**9+7
+        memo=[1]*10
+        
+        for _ in range(n-1):
+            new_memo=[0]*10
+            for old in range(10):
+                for new in adjs[old]:
+                    new_memo[new]+=memo[old]
+            memo=new_memo
+        
+        return sum(memo)%mod
