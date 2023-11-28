3a7f7111bd1b1382ca6a5d5e8eb6b55fbce37c5d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-11-28 18:46:28 -0500

    Time: 38 ms (67.61%), Space: 16.3 MB (79.18%) - LeetHub

---
+++ 0726-number-of-atoms/0726-number-of-atoms.py
@@ -0,0 +1,37 @@
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+        self.stack = [defaultdict(int)]
+        self.element = ''
+        self.count = 0
+        self.sub = None
+        
+        def calc():
+            if self.sub:
+                for ele,cnt in self.sub.items():
+                    self.stack[-1][ele] += cnt * (self.count or 1)
+            elif self.element:
+                self.stack[-1][self.element] += (self.count or 1)
+            self.element = ''
+            self.count = 0
+            self.sub = None
+            
+        for c in formula:
+            if c.isupper():
+                calc()
+                self.element += c
+            elif c.islower():
+                self.element += c
+            elif c.isdigit():
+                self.count = 10 * self.count + int(c)
+            elif c=='(':
+                calc()
+                self.stack.append(defaultdict(int))
+            elif c==')':
+                calc()
+                self.sub = self.stack.pop()
+        calc()
+
+        assert(len(self.stack)==1)
+        items = list(self.stack[-1].items())
+        items.sort()
+        return ''.join(element + (str(count) if count>1 else '') for element, count in items)
