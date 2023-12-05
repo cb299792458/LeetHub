1780a8800f1714f9ab860c1a1e1ecfd879070bff
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-12-04 20:04:23 -0500

    Time: 682 ms (5.06%), Space: 20.7 MB (20.74%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -3,13 +3,33 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+"""
+myDict = {myKey: myValue}
+
+head = {
+    val: 3,
+    next: {
+        val: 2,
+        next: {
+            val: 0,
+            next: {
+                val: -4,
+                next: None
+            }
+        }
+    }
+}
+"""
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
+        seen = []
         curr = head
+        
         while curr:
-            if curr in seen: return True
-            seen.add(curr)
+            if curr in seen:
+                return True
+            seen.append(curr)
             curr = curr.next
+        
         return False
