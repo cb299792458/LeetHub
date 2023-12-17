54980c8b2a7df7e996a124f6c651f69d9ac111c7
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2023-12-17 18:57:25 -0500

    Time: 713 ms (92.31%), Space: 47.2 MB (67.46%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -1,66 +1,19 @@
-class Food {
-public:
-    // Store the food's rating.
-    int foodRating;
-    // Store the food's name.
-    string foodName;
-
-    Food(int foodRating, string foodName) {
-        this->foodRating = foodRating;
-        this->foodName = foodName;
-    }
-
-    // Overload the less than operator for comparison
-    bool operator<(const Food& other) const {
-        // If food ratings are same sort on the basis of their name. (lexographically smaller name food will be on top)
-        if (foodRating == other.foodRating) {
-            return foodName > other.foodName;
-        }
-        // Sort on the basis of food rating. (bigger rating food will be on top)
-        return foodRating < other.foodRating;
-    }
-};
-
-class FoodRatings {
-    // Map food with its rating.
-    unordered_map<string, int> foodRatingMap;
-    // Map food with cuisine it belongs to.
-    unordered_map<string, string> foodCuisineMap;
-    
-    // Store all food of a cusine in priority queue (to sort them on ratings/name)
-    // Priority queue element -> Food: (foodRating, foodName)
-    unordered_map<string, priority_queue<Food>> cuisineFoodMap;
-
-public:
-    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        for (int i = 0; i < foods.size(); ++i) {
-            // Store 'rating' and 'cuisine' of current 'food' in 'foodRatingMap' and 'foodCuisineMap' maps.
-            foodRatingMap[foods[i]] = ratings[i];
-            foodCuisineMap[foods[i]] = cuisines[i];
-            // Insert the '(rating, name)' element in current cuisine's priority queue.
-            cuisineFoodMap[cuisines[i]].push(Food(ratings[i], foods[i]));
-        }
-    } 
-    
-    void changeRating(string food, int newRating) {
-        // Update food's rating in 'foodRating' map.
-        foodRatingMap[food] = newRating;
-        // Insert the '(new rating, name)' element in respective cuisine's priority queue.
-        auto cuisineName = foodCuisineMap[food];
-        cuisineFoodMap[cuisineName].push(Food(newRating, food));
-    }
-    
-    string highestRated(string cuisine) {
-        // Get the highest rated 'food' of 'cuisine'.
-        auto highestRated = cuisineFoodMap[cuisine].top();
+class FoodRatings:
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.ratings = dict()
+        self.cuisines = defaultdict(list)
         
-        // If latest rating of 'food' doesn't match with 'rating' on which it was sorted in priority queue,
-        // then we discard this element of the priority queue.
-        while (foodRatingMap[highestRated.foodName] != highestRated.foodRating) {
-            cuisineFoodMap[cuisine].pop();
-            highestRated = cuisineFoodMap[cuisine].top();
-        }
-        // Return name of the highest rated 'food' of 'cuisine'.
-        return highestRated.foodName;
-    }
-};
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.ratings[food] = (rating, cuisine)
+            heapq.heappush(self.cuisines[cuisine], (-rating, food))
+        
+    def changeRating(self, food: str, newRating: int) -> None:
+        cuisine = self.ratings[food][1]
+        self.ratings[food] = (newRating, cuisine)
+        heapq.heappush(self.cuisines[cuisine], (-newRating, food))
+        
+    def highestRated(self, cuisine: str) -> str:
+        # print(self.cuisines[cuisine][0][0] , -self.ratings[self.cuisines[cuisine][0][1]][0])
+        while self.cuisines[cuisine][0][0] != -self.ratings[self.cuisines[cuisine][0][1]][0]:
+            heapq.heappop(self.cuisines[cuisine])
+        return self.cuisines[cuisine][0][1]
