c4a70cc8b01b6febd0ba69e7f563c227ba66f503
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-05 12:04:23 -0500

    Time: 63 ms (29.23%), Space: 43.7 MB (12.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,21 +2,22 @@ var isValid = function(s) {
     const stack = [];
     
     for (let char of s) {
-        if (['(', '[', '{'].includes(char)) {
+        if ( ['(', '{', '['].includes(char)) {
             stack.push(char);
         } else {
             const last = stack[stack.length-1];
-            if (last==='(' && char===')') {
+            if (char===')' && last==='(') {
                 stack.pop();
-            } else if (last==='{' && char==='}') {
+            } else if (char==='}' && last==='{') {
                 stack.pop();
-            } else if (last==='[' && char===']') {
+            } else if (char===']' && last==='[') {
                 stack.pop();
             } else {
                 return false;
             }
         }
     }
+    
     if (stack.length>0) {
         return false;
     }
