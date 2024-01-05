23f1f708f3218101692f2ffe5262b3e95078794e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-05 11:27:34 -0500

    Time: 207 ms (5.00%), Space: 41.9 MB (92.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,13 +1,15 @@
 var twoSum = function(nums, target) {
     const N = nums.length;
-    const indexOfPrev = {}
     
     for(let i=0; i<N; i++){
-        const diff = target - nums[i];
-        if(diff in indexOfPrev){
-            return [indexOfPrev[diff], i];
+        for(let j=0; j<N; j++){
+            if(i===j){
+                continue;
+            }
+            
+            if(nums[i]+nums[j]===target){
+                return [i,j];
+            }
         }
-        indexOfPrev[nums[i]] = i
     }
-    
 };
