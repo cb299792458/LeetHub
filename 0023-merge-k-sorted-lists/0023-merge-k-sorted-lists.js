a3aa9ac638099c0e10f94664e011528bdf2a78ce
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-05 19:56:13 -0500

    Time: 355 ms (23.72%), Space: 47.1 MB (78.87%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -0,0 +1,25 @@
+var mergeKLists = function(lists) {
+    if(!lists.length) return null;
+    for(let i=1;i<lists.length;i++){
+        lists[0] = mergeTwoLists(lists[0],lists[i]);
+    }
+    return lists[0];
+};
+
+function mergeTwoLists(l1,l2){
+    let head = new ListNode();
+    let curr = head;
+    while(l1 && l2){
+        if(l1.val>l2.val){
+            curr.next = l2;
+            l2 = l2.next;
+        } else {
+            curr.next = l1;
+            l1 = l1.next;
+        }
+        curr = curr.next;
+    }
+    if(l1) curr.next = l1;
+    if(l2) curr.next = l2;
+    return head.next;
+}
