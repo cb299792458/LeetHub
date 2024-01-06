49a193d03a2d2a80762e6482170f5ebdfb1eaf45
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-05 19:54:29 -0500

    Time: 515 ms (98.89%), Space: 32.7 MB (44.76%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -4,11 +4,11 @@ class Solution:
         jobs = [(s,e,p) for [s,e,p] in zip(startTime,endTime,profits)]
         jobs.sort()
         
-        heap = []
+        heap = [] # sorted by end time, so all paths that end before start are considered
         for (s,e,p) in jobs:
             while heap and heap[0][0]<=s: # check all paths that end before new start
                 best = max(best, heapq.heappop(heap)[1]) # get best profit up to new job
                 
             heapq.heappush(heap,(e,best+p)) # add result of doing new job to heap
         
-        return max(t[1] for t in heap)
+        return max(t[1] for t in heap) # return best result at end of considering each job
