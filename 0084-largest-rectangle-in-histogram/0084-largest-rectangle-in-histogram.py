aa5e3bb39e8386af1e14cacbfbd7d1b20b1a540f
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-05 19:59:22 -0500

    Time: 766 ms (67.35%), Space: 30.9 MB (71.21%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,23 +1,6 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-#         # stack heights, calc all to left when right is smaller
-#         res=0
-#         stack=[]
-        
-#         heights = heights + [0]
-#         for i in range(len(heights)):
-#             w=0
-#             while stack and stack[-1]>heights[i]:
-#                 w+=1
-#                 h=stack.pop()
-#                 res = max(res,w*h)
-                
-#             stack += [heights[i]] * (w+1)
-                
-#         return res
 
-
-        # three passes, find indices of lower heights to left and right
         res=0
         heights.append(0)
         next_smaller = [-1 for _ in range(len(heights))]
@@ -40,7 +23,6 @@ class Solution:
         for i in range(len(heights)):
             height = heights[i]
             width = next_smaller[i] - prev_smaller[i] - 1
-            # print(next_smaller[i],prev_smaller[i],width)
             res = max(res,height*width)
         
         return res
