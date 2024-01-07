cb92cb9983334d88ba58dc0f3fe693d2c82c92ac
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-06 20:27:45 -0500

    Time: 696 ms (43.53%), Space: 71.9 MB (26.98%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -4,10 +4,16 @@ class Solution:
         N = len(nums)
         count = 0
         
-        for i in range(1,N):
-            for j in range(i):
+        for i in range(1,N): # 3rd num
+            for j in range(i): # 2nd num
                 diff = nums[j]-nums[i]
+                
+                # one more sequence ending at i
+                # than sequences ending at j w/ same diff
                 dp[i][diff] += dp[j][diff] + 1
+                
+                # if there were sequences ending at j
+                # all those sequences have length >=3 at i
                 count += dp[j][diff]
         
         return count
