9b634cb1e84c9cb309fd40b2b30ea881c57c3af4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-08 14:11:43 -0500

    Time: 110 ms (80.34%), Space: 58.8 MB (26.55%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function(accounts) {
+    const owners = {};
+    const emails = [];
+    
+    for(let i in accounts){
+        emails[i] = []
+        for(let j=1;j<accounts[i].length;j++){
+            let email = accounts[i][j];
+            if(!owners[email]) owners[email] = [];
+            owners[email].push(i);
+            emails[i].push(email);
+        }
+    }
+    
+    let seen = new Set();
+    let mergedAccounts = [];
+    
+    for(let i in accounts){
+        if(seen.has(i)) continue;
+        
+        let name = accounts[i].shift();
+        let userEmails = new Set(accounts[i]);
+        userEmails.forEach((email)=>{
+            for(let owner of owners[email]){
+                seen.add(owner)
+                for(let linkedEmail of emails[owner]){
+                    userEmails.add(linkedEmail);
+                }
+            }
+        });
+        
+        
+        let emailArray = Array.from(userEmails);
+        emailArray.sort((a,b)=>{
+            if(a<b) return -1;
+            return 1;
+        });
+        
+        mergedAccounts.push([name,...emailArray]);
+    }
+    return mergedAccounts;
+};
