8c1b29cf0fc613d7e9dc6db1a59476b675669a6d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-13 16:26:56 -0500

    Time: 803 ms (13.77%), Space: 128.6 MB (14.92%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        S, P = len(s), len(p)
+        
+        @cache
+        def match(si,pi):
+            print(si,pi)
+            if pi==P:
+                return si==S
+            if si==S:
+                return all(c=='*' for c in p[pi:])
+            
+            if p[pi]=='*':
+                return match(si,pi+1) or match(si+1,pi+1) or match(si+1,pi)
+            
+            elif p[pi]=='?':
+                return match(si+1,pi+1)
+            else:
+                if p[pi]==s[si]:
+                    return match(si+1,pi+1)
+                else:
+                    return False
+        
+        return match(0,0)
