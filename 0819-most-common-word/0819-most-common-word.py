69494f9a944c33112610108527c7d4a137f7515c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-18 14:06:56 -0500

    Time: 41 ms (68.56%), Space: 17.3 MB (25.98%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,15 +1,18 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        symbols = "!?',;."
-        for s in symbols:
-            paragraph = paragraph.replace(s, ' ')
-        paragraph = paragraph.lower()
-            
         counts = Counter()
-        best = ('', 0)
-        for word in paragraph.split():
-            if word not in banned:
-                counts[word] += 1
-                if counts[word] > best[1]:
-                        best = (word, counts[word])
+        best = ('', 0) # word, frequency
+        banned = set(banned)
+        
+        curr = ''
+        for char in list(paragraph+' '):
+            if not char.isalpha():
+                if curr and curr not in banned:
+                    counts[curr] += 1
+                    if counts[curr] > best[1]:
+                        best = (curr, counts[curr])
+                curr=''
+            else:
+                curr += char.lower()
+        
         return best[0]
