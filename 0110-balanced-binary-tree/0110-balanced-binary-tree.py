2cf6923cc2b5c72adad2c8843381eb602bdf03f9
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-18 14:55:47 -0500

    Time: 44 ms (85.64%), Space: 18.5 MB (89.71%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,26 +6,31 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def find_height(node):
+        def find_heights(node):
             if not node:
                 return 0
-            height = max(find_height(node.left),find_height(node.right)) + 1
+            height = max(find_heights(node.left), find_heights(node.right)) + 1
             node.height = height
             return height
-        find_height(root)
         
-        def check_balance(node):
-            if not node:
+        find_heights(root)
+        
+        def check_balance(node) -> bool:
+            if not node: # node doesn't exist
                 return True
-            if not node.left and not node.right:
-                return True
-            if node.left and not node.right:
-                return node.left.height < 2
-            if node.right and not node.left:
-                return node.right.height < 2
             
-            if abs(node.left.height - node.right.height) > 1:
+            if not node.left and not node.right: # node's children don't exist
+                return True
+            
+            if node.left and not node.right: # one child doesn't exist
+                return node.left.height == 1
+            
+            if node.right and not node.left: # one child doesn't exist
+                return node.right.height == 1
+                
+            if abs(node.right.height - node.left.height) >= 2:
                 return False
+            
             return check_balance(node.left) and check_balance(node.right)
         
         return check_balance(root)
