621963d4affc4d433d6ce537578868716d577e1c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-20 11:37:32 -0500

    Time: 326 ms (89.95%), Space: 21.9 MB (61.29%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,8 +1,8 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         total = 0
-        stack = [(-1,0)] # idx, num
-        for i,n in enumerate(arr+[0]):
+        stack = [(-1,float('-inf'))] # idx, num
+        for i,n in enumerate(arr+[float('-inf')]):
             while n < stack[-1][1]:
                 (j,m) = stack.pop()
                 l = j - stack[-1][0]
