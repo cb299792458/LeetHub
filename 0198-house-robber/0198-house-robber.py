014b093ac5d37c1721b6b1017ef68b734bc8d71e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-20 19:18:59 -0500

    Time: 36 ms (71.38%), Space: 16.4 MB (60.22%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [nums[0], max(nums[:2])]
+        
+        for i in range(2, len(nums)):
+            memo.append(max(memo[-1],nums[i]+memo[-2]))
+        
+        return max(memo[-2:])
