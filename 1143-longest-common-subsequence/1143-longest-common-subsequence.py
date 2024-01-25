48889129fc36045f6147887388d5508f5259da29
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-25 09:29:24 -0500

    Time: 466 ms (88.00%), Space: 42.5 MB (55.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        text1 = '#' + text1
-        text2 = '*' + text2
+        text1 = ' ' + text1
+        text2 = ' ' + text2
         m,n = len(text1), len(text2)
         
         dp = [[0 for _ in range(n)] for _ in range(m)]
@@ -12,6 +12,6 @@ class Solution:
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        print(dp)
+
         return dp[-1][-1]
                                                     
