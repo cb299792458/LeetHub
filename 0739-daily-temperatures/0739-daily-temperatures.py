74d4ac4ef7589bccabad4bffb318f5fb81d4ac9f
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-01-31 11:10:17 -0500

    Time: 882 ms (87.77%), Space: 30.5 MB (92.24%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,14 +2,15 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         N = len(temperatures)
         res = [0] * N
-        stack = []
+        stack = [] # [greater_temp, index]
         for i in range(N-1, -1, -1):
             temp = temperatures[i]
-            while stack and stack [-1][0] <= temp:
-                stack.pop()
+            while stack and stack [-1][0] <= temp: # if current is warmer/closer
+                stack.pop() # replace elements in stack with current
             
             if stack:
                 res[i] = stack[-1][1] - i
+                
             stack.append((temp, i))
             
         return res
