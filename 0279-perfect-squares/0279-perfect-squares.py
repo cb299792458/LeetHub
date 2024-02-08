ac7a6fa4c88d9fcf4fd40367f408b830cab0bbd5
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-02-08 16:49:30 -0500

    Time: 4715 ms (10.82%), Space: 38.7 MB (8.75%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,21 +1,15 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        best = n
-        squares = [i*i for i in range(1,int(sqrt(n)+1))]
         
         @cache
-        def dp(total, nums):
-            if total==n:
-                nonlocal best
-                best = min(nums, best)
-                return
-            if total>n:
-                return
-            if nums==4:
-                return
-            
-            for num in squares:
-                dp(total+num, nums+1)
+        def bfs(num):
+            if num==0:
+                return 0
+            if num<0:
+                return n
+            best = n
+            for i in range(1, int(n**0.5)+1):
+                best = min(best, bfs(num-i*i)+1)
+            return best
         
-        dp(0,0)
-        return best
+        return bfs(n)
