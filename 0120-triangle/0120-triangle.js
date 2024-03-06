7e88068283b663d20ef77b470d8d54a500e731aa
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-03-05 20:03:13 -0500

    Time: 61 ms (41.07%), Space: 49.4 MB (55.76%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -1,11 +1,12 @@
 const minimumTotal = (triangle) => {
     for (let i=1; i<triangle.length; i++) {
-        const row = triangle[i];
-        for (let j=0; j<row.length; j++) {
+        const lastRow = triangle[i-1];
+        const thisRow = triangle[i];
+        for (let j=0; j<thisRow.length; j++) {
             if (j===0) {
-                triangle[i][j] += triangle[i-1][0];
-            } else if (j===row.length-1) {
-                triangle[i][j] += triangle[i-1][j-1];
+                triangle[i][j] += lastRow[j];
+            } else if (j===thisRow.length-1) {
+                triangle[i][j] += lastRow[j-1];
             } else {
                 triangle[i][j] += Math.min(
                     triangle[i-1][j],
