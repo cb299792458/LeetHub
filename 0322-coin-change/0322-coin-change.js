c9e797abbda8770b06ffcc1497c79cad80e8295d
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-03-06 09:27:38 -0500

    Time: 117 ms (39.96%), Space: 55.7 MB (35.76%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount, memo = {}) {
+    if(amount===0) return 0;
+    if(amount < 0) return -1;
+    if(memo[amount]) return memo[amount];
+    
+    let res = Infinity;
+    
+    for(const coin of coins){
+        const newAmount = amount - coin;
+        const newAns = coinChange(coins, newAmount, memo);
+        if(newAns !== -1){
+            res = Math.min(res,newAns+1)
+        }
+    }
+    memo[amount] = res;
+    return res===Infinity ? -1 : res;
+};
