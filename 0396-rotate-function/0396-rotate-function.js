f2d07d51c52d6b3bcdb01ee3f6d41689f7b18878
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-03-17 16:57:00 -0400

    Time: 72 ms (89.58%), Space: 60.3 MB (50.00%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxRotateFunction = function(nums) {
+    let sum = 0;
+    let val = 0;
+    for (let i=0;i<nums.length;i++) {
+        val += i * nums[i];
+        sum += nums[i];
+    }
+    let best = val;
+    
+    for (let j=nums.length-1;j>0;j--) {
+        val += sum;
+        val -= (nums.length) * nums[j];
+        best = Math.max(best, val)
+    }
+    
+    return best
+    
+};
