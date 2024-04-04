32e4ffb0133c4e7875da4d70421d70258d17779c
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-04-04 13:26:50 -0400

    Time: 335 ms (42.23%), Space: 58.9 MB (44.86%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.js
@@ -0,0 +1,27 @@
+var ladderLength = function(beginWord, endWord, wordList) {
+    wordList = new Set(wordList);
+    if(!wordList.has(endWord)) return 0;
+    
+    let currentWords = new Set([beginWord]);
+    let steps = 2;
+    const alphabet = 'qwertyuiopasdfghjklzxcvbnm';
+    
+    while(currentWords.size){
+        let temp = new Set();
+        for(let word of currentWords){
+            wordList.delete(word);
+    
+            for(let i=0;i<word.length;i++){
+                for(let letter of alphabet){
+                    let newWord = word.slice(0,i) + (letter) + word.slice(i+1);
+
+                    if(newWord===endWord) return steps;
+                    if(wordList.has(newWord)) temp.add(newWord);
+                }
+            }
+        }
+        currentWords = temp;
+        steps++;
+    }
+    return 0;
+};
