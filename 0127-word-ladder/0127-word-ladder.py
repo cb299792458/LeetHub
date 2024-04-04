23d76c937fd279392d0659a4a86c576265631af2
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-04-04 13:39:01 -0400

    Time: 8248 ms (5.18%), Space: 17.5 MB (98.00%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,26 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        def adj(beg, end):
+            diffs = 0
+            for c1, c2 in zip(beg,end):
+                if c1!=c2:
+                    diffs+=1
+            return diffs<2
+        
+        steps = dict()
+        steps[beginWord] = 1
+        
+        queue = deque([beginWord])
+        
+        while queue:
+            curr = queue.popleft()
+            for neighbor in wordList:
+                if neighbor in steps:
+                    continue
+                if not adj(curr,neighbor):
+                    continue
+                
+                steps[neighbor] = steps[curr] + 1
+                queue.append(neighbor)
+
+        return steps[endWord] if endWord in steps else 0
