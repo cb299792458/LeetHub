7a8387969a2769c73021292dc86413fcbb299d72
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-04-04 13:25:14 -0400

    Time: 324 ms (31.03%), Space: 16.5 MB (83.42%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        def fibonacci(n):
+            if n<2: return n
+            return fibonacci(n-1) + fibonacci(n-2)
+        
+        return fibonacci(n)
