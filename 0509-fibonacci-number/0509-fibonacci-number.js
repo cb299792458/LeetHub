625b84c7f9aba228e683a1aed0c2dd47601ac994
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-04-04 20:26:02 -0400

    Time: 46 ms (90.37%), Space: 48.3 MB (88.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,8 +2,13 @@
  * @param {number} n
  * @return {number}
  */
+const answers = {0: 0, 1: 1}
 var fib = function(n) {
-    if (n===0) return 0
-    if (n===1) return 1
-    return fib(n-1) + fib(n-2)
+    if (n in answers) {
+        return answers[n];
+    }
+    
+    const result = fib(n-1) + fib(n-2);
+    answers[n] = result;
+    return result
 };
