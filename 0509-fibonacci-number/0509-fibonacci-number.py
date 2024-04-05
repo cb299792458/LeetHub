a58241fa543d2508a36f72c6b0d70e4a7145e56e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-04-04 20:26:35 -0400

    Time: 328 ms (29.23%), Space: 16.4 MB (83.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
         
-        @cache
+        # @cache
         def fibonacci(n):
             if n<2: return n
             return fibonacci(n-1) + fibonacci(n-2)
