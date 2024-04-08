66696fd4630e51aa851389bc1de622912f0cc76e
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-04-08 15:12:21 -0400

    Time: 77 ms (41.54%), Space: 51.5 MB (94.08%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,16 +4,19 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    const counts = {};
-    for (const c of magazine) {
-        counts[c] ||= 0;
+    const counts = {}; // key: character, val: count for that character
+    for (let c of magazine) {
+        if (counts[c] === undefined) {
+            counts[c] = 0;
+        }
         counts[c]++;
     }
-    for (const c of ransomNote) {
+    
+    for (let c of ransomNote) {
         if (counts[c] === undefined || counts[c] === 0) {
             return false;
         }
         counts[c]--;
     }
-    return true;
+    return true
 };
