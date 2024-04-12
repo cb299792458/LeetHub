98a50e345c549e1e6aaeb435b4beef0d0afd0da3
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-04-12 12:53:30 -0400

    Time: 59 ms (61.70%), Space: 53.3 MB (25.80%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,19 @@
+var trap = function(height) {
+    let length = height.length;
+    let leftWall = new Array(length);
+    let rightWall = new Array(length);
+    leftWall[0] = 0;
+    rightWall[length-1] = 0;
+    for(let i=1;i<length;i++){
+        leftWall[i] = Math.max(leftWall[i-1],height[i-1]);
+    }
+    for(let j=length-2;j>=0;j--){
+        rightWall[j] = Math.max(rightWall[j+1],height[j+1]);
+    }
+    
+    let area = 0;
+    for(let k=0;k<length;k++){
+        area += Math.max(0,Math.min(leftWall[k],rightWall[k])-height[k])
+    }
+    return area;
+};
