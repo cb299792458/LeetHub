c07fb085975aeb26ef0d2834f8aa35ac63a6a132
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-07-05 22:28:52 -0400

    Time: 38 ms (35.95%), Space: 16.5 MB (77.72%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,7 +1,8 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
+        loop = 2*(n-1)
         if time < n:
             return time + 1
-        if time < 2*(n-1):
-            return (2*n) - time - 1
-        return self.passThePillow(n, time-(2*(n-1)))
+        if time < loop:
+            return loop - time + 1
+        return self.passThePillow(n, time-loop)
