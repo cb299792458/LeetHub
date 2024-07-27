23a1727b60f6b052804bc44b8ffe1c4a69a55e17
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-07-27 13:05:13 -0400

    Time: 2502 ms (20.81%), Space: 18.2 MB (58.38%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -2,18 +2,18 @@ class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
         INF = 10 ** 20
         alphabet = 'qwertyuiopasdfghjklzxcvbnm'
-        costs = defaultdict(lambda: defaultdict(lambda: INF))
+        prices = defaultdict(lambda: defaultdict(lambda: INF))
         
         for c in alphabet:
-            costs[c][c] = 0
+            prices[c][c] = 0
         
         for (old, new, price) in zip(original, changed, cost):
-            costs[old][new] = min(costs[old][new], price)
+            prices[old][new] = min(prices[old][new], price)
             
         for a in alphabet:
             for b in alphabet:
                 for c in alphabet:
-                    costs[b][c] = min(costs[b][c], costs[b][a] + costs[a][c])
+                    prices[b][c] = min(prices[b][c], prices[b][a] + prices[a][c])
         
-        res = sum(costs[s][t] for (s,t) in zip(source,target))
+        res = sum(prices[s][t] for (s,t) in zip(source,target))
         return res if res < INF else -1
