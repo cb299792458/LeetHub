43306cec4b5b34a8dd658d1eb7fc0811802b5eb4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-08-04 10:01:43 -0400

    Time: 3030 ms (9.29%), Space: 39.9 MB (51.33%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -7,5 +7,4 @@ class Solution:
                 subarray_sums.append(sum(nums[i:j]))
         
         subarray_sums.sort()
-        print(subarray_sums)
         return sum(subarray_sums[left-1:right]) % (10**9 + 7)
