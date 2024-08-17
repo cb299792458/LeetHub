72a8a0842354988c2192454b54d182e7062b07ac
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-08-17 17:37:33 -0400

    Time: 355 ms (65.09%), Space: 22.2 MB (50.31%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        best_pair = 0
+        best_spot = 0
+        
+        for i, val in enumerate(values):
+            best_spot -= 1
+            best_pair = max(best_pair, val + best_spot)
+            best_spot = max(best_spot, val)
+        
+        return best_pair
