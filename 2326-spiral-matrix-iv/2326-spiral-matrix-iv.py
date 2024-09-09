5e0dfd664dc58abfa3dc2365b4974d574480dea4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-09-08 20:56:10 -0400

    Time: 647 ms (74.20%), Space: 54.9 MB (63.04%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -12,28 +12,27 @@ class Solution:
             (0,-1),
             (-1,0),
         ]
-        di = r = c = r_min = c_min = 0
-        r_min+=1
+        di = r = c = c_min = 0
+        r_min = 1
         
         curr = head
         while curr:
-            # print(r,c)
             res[r][c] = curr.val
             
-            if di%4==0 and c==n-1:
+            if di==0 and c==n-1:
                 di+=1
                 n-=1
-            elif di%4==1 and r==m-1:
+            elif di==1 and r==m-1:
                 di+=1
                 m-=1
-            elif di%4==2 and c==c_min:
+            elif di==2 and c==c_min:
                 di+=1
                 c_min+=1
-            elif di%4==3 and r==r_min:
-                di+=1
+            elif di==3 and r==r_min:
+                di=0
                 r_min+=1
             
-            r,c = r+dirs[di%4][0],c+dirs[di%4][1]
+            r,c = r+dirs[di][0],c+dirs[di][1]
             curr = curr.next
         
         return res
