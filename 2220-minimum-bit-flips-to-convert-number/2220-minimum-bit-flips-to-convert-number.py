0fbfe65249074113ce6f2e98bbd694e9e8c63890
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-09-11 20:36:30 -0400

    Time: 30 ms (86.84%), Space: 16.4 MB (69.14%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,4 +1,4 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         xor = bin(start ^ goal)
-        return sum(1 if c=='1' else 0 for c in xor)
+        return sum(int(c=='1') for c in xor)
