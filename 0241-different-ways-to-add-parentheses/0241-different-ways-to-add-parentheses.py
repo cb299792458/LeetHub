fac8fec02dd2236e91ee97d6419a5bec93f4a250
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-09-19 18:31:42 -0400

    Time: 43 ms (26.64%), Space: 16.6 MB (90.55%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -0,0 +1,35 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        stack = []
+        for char in expression:
+            if char.isdigit():
+                if stack and isinstance(stack[-1],int):
+                    stack[-1] = stack[-1]*10 + int(char)
+                else:
+                    stack.append(int(char))
+            else:
+                stack.append(char)
+        
+        def helper(stack):
+            if len(stack)==1:
+                assert(isinstance(stack[0],int))
+                return stack
+            
+            results = []
+            
+            for [i,c] in enumerate(stack):
+                if c=='+' or c=='*' or c=='-':
+                    lefts = helper(stack[:i])
+                    rights = helper(stack[i+1:])
+                    for l in lefts:
+                        for r in rights:
+                            if c=='+':
+                                results.append(l+r)
+                            if c=='*':
+                                results.append(l*r)
+                            if c=='-':
+                                results.append(l-r)
+            return results
+        
+        return helper(stack)
+                
