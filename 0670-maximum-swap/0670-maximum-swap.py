518c64a06fa3c4d8b05eddf3dd980e1d890c0f3a
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-10-18 13:20:23 -0400

    Time: 0 ms (100.00%), Space: 16.5 MB (42.71%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -4,10 +4,10 @@ class Solution:
         num = str(num)
         N = len(num)
         big = N-1
-        for i in range(N-2,-1,-1):
+        for i in range(N-2,-1,-1): # go backwards
             if int(num[i])>int(num[big]):
-                big = i
-            elif int(num[i])<int(num[big]):
+                big = i # save idx of biggest digit
+            elif int(num[i])<int(num[big]): # swap digits and save res
                 res = int(num[:i] + num[big] + num[i+1:big] + num[i] + num[big+1:])
         
         return res
