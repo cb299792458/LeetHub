4314e8d7f4ad846f9611c3d8a28e997609cb66d3
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-10-18 13:19:32 -0400

    Time: 0 ms (100.00%), Space: 16.7 MB (10.93%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,19 +1,13 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        # Convert the number to a list of characters for easy manipulation
-        num_list = list(str(num))
+        res = num
+        num = str(num)
+        N = len(num)
+        big = N-1
+        for i in range(N-2,-1,-1):
+            if int(num[i])>int(num[big]):
+                big = i
+            elif int(num[i])<int(num[big]):
+                res = int(num[:i] + num[big] + num[i+1:big] + num[i] + num[big+1:])
         
-        # Track the last occurrence of each digit (0-9)
-        last = {int(d): i for i, d in enumerate(num_list)}
-        
-        # Traverse the number from left to right
-        for i, digit in enumerate(num_list):
-            # Check for a larger digit to swap
-            for d in range(9, int(digit), -1):
-                if last.get(d, -1) > i:
-                    # Swap and return the new number
-                    num_list[i], num_list[last[d]] = num_list[last[d]], num_list[i]
-                    return int(''.join(num_list))
-        
-        # If no swap occurred, return the original number
-        return num
+        return res
