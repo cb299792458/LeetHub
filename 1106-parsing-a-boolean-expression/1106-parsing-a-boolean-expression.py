c1b9b6c91c1ea788453ad06659377ea5c57d6fbf
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-10-20 09:42:02 -0400

    Time: 15 ms (100.00%), Space: 16.6 MB (95.21%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
@@ -0,0 +1,23 @@
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        stack = []
+        
+        for c in expression:
+            if c==')':
+                sub_exp = []
+                while stack[-1]!='(':
+                    sub_exp.append(stack.pop())
+                stack.pop()
+                op = stack.pop()
+                
+                match op:
+                    case '!':
+                        stack.append('t' if sub_exp[0]=='f' else 'f')
+                    case '&':
+                        stack.append('t' if all(c=='t' for c in sub_exp) else 'f')
+                    case '|':
+                        stack.append('t' if any(c=='t' for c in sub_exp) else 'f')
+            elif c!=',':
+                stack.append(c)
+        
+        return stack[0]=='t'
