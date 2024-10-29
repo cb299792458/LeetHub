2c952a08eea9555a15f44447345d4e99602c1283
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-10-29 09:08:02 -0400

    Time: 121 ms (72.73%), Space: 43 MB (5.30%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -3,18 +3,18 @@ class Solution:
         M, N = len(grid), len(grid[0])
         
         @cache
-        def search(r,c):
+        def dfs(r,c):
             if c==N-1:
                 return 0
             
             top, mid, bot = 0, 0, 0
-            if r<M-1 and grid[r][c]<grid[r+1][c+1]:
-                top += 1 + search(r+1,c+1)
-            if grid[r][c]<grid[r][c+1]:
-                mid += 1 + search(r,c+1)
             if r>0 and grid[r][c]<grid[r-1][c+1]:
-                bot = 1 + search(r-1,c+1)
+                top = 1 + dfs(r-1,c+1)
+            if grid[r][c]<grid[r][c+1]:
+                mid += 1 + dfs(r,c+1)
+            if r<M-1 and grid[r][c]<grid[r+1][c+1]:
+                bot += 1 + dfs(r+1,c+1)
         
             return max(top, mid, bot)
         
-        return max(search(r,0) for r in range(M))
+        return max(dfs(r,0) for r in range(M))
