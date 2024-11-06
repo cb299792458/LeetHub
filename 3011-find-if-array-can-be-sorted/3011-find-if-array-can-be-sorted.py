f3d2476fa15ed0ea0b1a87a670e7352645c2e5b5
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-11-05 22:58:03 -0500

    Time: 8 ms (64.55%), Space: 16.8 MB (23.35%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        def bit_count(num):
+            bits = 0
+            while num:
+                bits += num%2
+                num //= 2
+            return bits
+        
+        curr_bits = 0
+        last_high = 0
+        curr_high = 0
+        
+        for num in nums:
+            if num<last_high:
+                return False
+            bits = bit_count(num)
+                        
+            if bits == curr_bits:
+                curr_high = max(num, curr_high)
+            else:
+                last_high = curr_high
+                curr_high = num
+                curr_bits = bits
+                if num<last_high:
+                    return False
+        
+        return True
