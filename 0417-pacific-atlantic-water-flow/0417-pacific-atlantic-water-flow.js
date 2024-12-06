2dc6e60633cb956fb273fa18f0409434ab2ee1a4
Brian Lam <104156640+cb299792458@users.noreply.github.com>
2024-12-06 15:02:58 -0500

    Time: 38 ms (52.58%), Space: 60.8 MB (26.47%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+var pacificAtlantic = function(heights) {
+    
+    const maxRow = heights.length-1;
+    const maxCol = heights[0].length-1;
+    const outOfBounds = (r,c) => {
+        return r < 0 || r > maxRow || c < 0 || c > maxCol;
+    };
+    
+    const dirs = [[1,0],[0,1],[-1,0],[0,-1]];
+    
+    const pSquares = new Set;
+    const aSquares = new Set;
+    
+    const dfs = (r,c,memo) => {
+        const coords = `${r},${c}`;
+        if(memo.has(coords)) return;
+        memo.add(coords);
+        
+        for(let dir of dirs){
+            const newRow = r + dir[0];
+            const newCol = c + dir[1];
+            if(outOfBounds(newRow,newCol)) continue;
+            if(heights[r][c] > heights[newRow][newCol]) continue;
+            
+            dfs(newRow,newCol,memo);
+        }
+    };
+    
+    const res = [];
+    
+    for(let i = 0; i <= maxRow; i++){
+        for(let j = 0; j <= maxCol; j++){
+            const coords = `${i},${j}`;
+            if(i===0 || j===0) dfs(i,j,pSquares);
+            if(i===maxRow||j===maxCol) dfs(i,j,aSquares);
+        }
+    }
+    
+    for(let i = 0; i <= maxRow; i++){
+        for(let j = 0; j <= maxCol; j++){
+            const coords = `${i},${j}`;
+            
+            if(pSquares.has(coords)&&aSquares.has(coords)){
+                res.push([i,j]);
+            }
+        }
+    }
+        
+    return res
+
+}
+
+
+
+
